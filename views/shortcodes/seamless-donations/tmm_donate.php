614202b26878cd0f647b48a5c6c3fbf7cf1b1106
Stanislav Samoilenko <st.prog.1986@gmail.com>
2015-03-16 13:07:41 +0200

    dip-110 Create defining checkbox and radio button elements script with a few rules to apply

--- views/shortcodes/seamless-donations/tmm_donate.php
+++ views/shortcodes/seamless-donations/tmm_donate.php
@@ -1,2 +1,39 @@
-<?php
-echo do_shortcode('[dgx-donate]');
+<?php echo do_shortcode('[dgx-donate]'); ?>
+
+<script type="text/javascript">
+	(function($) {
+		$(function() {
+
+			var donate_form = $('#dgx-donate-form');
+
+			donate_form.find('input[type="checkbox"]').each(function(){
+
+				if (!$(this).hasClass('tmm-checkbox')) {
+
+					var text = $(this).parent().contents().filter(function() {
+									return this.nodeType === 3;
+								});
+
+					text.wrap('<label></label>');
+					$(this).addClass('tmm-checkbox').next();
+				}
+
+			});
+
+			donate_form.find('input[type="radio"]').each(function(){
+
+				if (!$(this).hasClass('tmm-radio')) {
+
+					var text = $(this).parent().contents().filter(function() {
+						return this.nodeType === 3;
+					});
+
+					text.wrap('<label></label>');
+					$(this).addClass('tmm-radio').next();
+				}
+
+			});
+
+		});
+	}(jQuery));
+</script>
