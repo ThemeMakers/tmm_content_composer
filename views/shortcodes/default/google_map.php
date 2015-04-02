719124c0df28fdf0aa2725d91c3e05656b0696fd
Julia Samoilenko <jullsum88@gmail.com>
2015-04-02 15:44:18 +0300

    Debug checks

--- views/shortcodes/default/google_map.php
+++ views/shortcodes/default/google_map.php
@@ -35,7 +35,7 @@ if (isset($latitude) && $latitude !== '' && isset($longitude) && $longitude !== 
 
 		<script type="text/javascript">
 			jQuery(function() {
-				gmt_init_map(<?php echo $latitude ?>,<?php echo $longitude ?>, "google_map_<?php echo $inique_id ?>", <?php echo $zoom ?>, "<?php echo $maptype ?>", "<?php echo $content ?>", "<?php echo $enable_marker ?>", "<?php echo $enable_popup ?>", "<?php echo $enable_scrollwheel ?>",<?php echo $js_controls ?>, "<?php echo @$marker_is_draggable ?>");
+				gmt_init_map(<?php echo $latitude ?>,<?php echo $longitude ?>, "google_map_<?php echo $inique_id ?>", <?php echo $zoom ?>, "<?php echo $maptype ?>", "<?php echo $content ?>", "<?php echo $enable_marker ?>", "<?php echo $enable_popup ?>", "<?php echo $enable_scrollwheel ?>",<?php echo $js_controls ?>, "<?php echo (isset($marker_is_draggable)) ? $marker_is_draggable : ''; ?>");
 			});
 		</script>
 
