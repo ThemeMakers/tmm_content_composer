e6eafc3a6f7da453dba185753f5259087c263a25
Stanislav Samoilenko <st.prog.1986@gmail.com>
2015-06-16 16:41:31 +0300

    seamless donations 4.0 fixes

--- views/shortcodes/seamless-donations/tmm_donate.php
+++ views/shortcodes/seamless-donations/tmm_donate.php
@@ -1,1 +1,7 @@
-<?php echo do_shortcode('[dgx-donate]'); ?>
+<?php
+if (shortcode_exists('seamless-donations')) {
+	echo do_shortcode('[seamless-donations]');
+} else {
+	echo do_shortcode('[dgx-donate]');
+}
+
