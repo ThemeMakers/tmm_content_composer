6baa96d0e4efcb98998344a6029211fe891bc464
Stanislav Samoilenko <st.prog.1986@gmail.com>
2015-08-04 18:49:19 +0300

    eng-192 Product Slider shortcode - add price to caption

--- views/shortcodes/woocommerce/tmm_product_slider.php
+++ views/shortcodes/woocommerce/tmm_product_slider.php
@@ -79,10 +79,15 @@ if ( $products->have_posts() ) {
 		$products->the_post();
 		$uniqid = uniqid();
 		global $post;
+		global $product;
 		$slides[$uniqid] = array(
 			'imgurl' => esc_url(TMM_Helper::get_post_featured_image($post->ID, '')),
-			'sequence_content' => '<h1 class="sequence-slogan" style="text-align: center;"><a href="' . get_the_permalink($post->ID) . '"><span style="color: #f8b637;">' . get_the_title() . '</span></a></h1>',
-			//'sequence_content' => '[title type="h2" font_size="default" letter_spacing="" font_weight="400" align="center" font_family="Raleway" title_type="default" color="#ffffff" bottom_indent="" text_transform="0" bg_color="#f59611" bg_opacity="0.9" bg_radius="50" bg_padding="" bg_width="400" bg_height="400" use_general_color="0" bg_center="1" title_effect="none" word_animate="1" separate_row="Unique^Progressive^Power" sc_id="sc871398551196"]Unique Progressive Power[/title]',
+			'sequence_content' => '<h1 class="sequence-slogan" style="text-align: center;">
+										<a href="' . get_the_permalink($post->ID) . '">
+											<span style="color: #f8b637;">' . get_the_title() . '</span>
+											<p class="price">' . $product->get_price_html() . '</p>
+										</a>
+									</h1>',
 		);
 	}
 }
