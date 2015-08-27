d4c894d7b21e6c3979550bbcf78ec00cd732b36b
Julia Samoilenko <jullsum88@gmail.com>
2015-08-27 12:56:47 +0300

    added interpress shortcodes

---
+++ views/shortcodes/interpress/authors.php
@@ -0,0 +1,121 @@
+<?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
+
+		<?php
+		if ($userid=='all'){
+			$args = array(
+				'fields' => array('ID', 'user_nicename'),
+				'who' => 'author'
+				);
+			$users = get_users($args);
+			if (!empty($users)){
+				?>
+				<ul class="our-authors-list">
+					<?php
+					foreach ($users as $user) {
+						$user = get_userdata($user->ID);
+						$users_array[$user->ID] = $user->display_name;
+						if (is_object($user)){
+							?>
+							<li>
+								<div class="our-author-image">
+									<?php echo get_avatar($user->user_email, 130); ?>
+								</div><!--/ .author-image-->
+
+								<div class="our-author-entry">
+									<h5 class="our-author-name"><?php echo $user->display_name ?></h5>
+
+									<p>
+										<?php echo stripslashes($user->description); ?>
+									</p>
+
+									<?php TMM_USERS::my_author_social_links($user->ID); ?>
+
+								</div><!--/ .our-author-entry-->
+
+							</li>
+						<?php
+						}
+					}
+					?>
+				</ul>
+
+				<?
+			}
+
+		}else{
+
+			$user = get_userdata($userid); ?>
+
+				<?php if (is_object($user)){ ?>
+
+				<section class="section section-content padding-off">
+
+					<h5 class="simple-title"><?php _e('About The Author', TMM_CC_TEXTDOMAIN) ?></h5>
+
+					<div class="author-entry clearfix">
+
+						<div class="our-author-image">
+							<?php echo get_avatar($user->user_email, 130); ?>
+						</div><!--/ .author-image-->
+
+						<div class="our-author-entry">
+							<h5 class="our-author-name"><?php echo $user->display_name ?></h5>
+
+							<p>
+								<?php echo stripslashes($user->description); ?>
+							</p>
+
+							<?php TMM_USERS::my_author_social_links($userid); ?>
+
+						</div><!--/ .our-author-entry-->
+
+					</div><!--/ .author-entry-->
+
+					<?php } ?>
+
+				</section><!--/ .section-->
+
+				<section class="section padding-off">
+
+					<?php
+					global $post;
+					$path = $path = 'content/blog-medium/content';
+					$user_query = array(
+						'post_status' => array('publish'),
+						'posts_per_page' => 3,
+						'author' => $userid
+					);
+
+					$user_query['posts_per_page'] = (!empty($posts_per_page)) ? $posts_per_page : '-1';
+					$user_query['paged'] = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+					global $wp_query;
+					$original_query = $wp_query;
+					$wp_query = new WP_Query($user_query);
+					?>
+
+					<?php if (have_posts()){  ?>
+
+					<div class="post-list">
+
+						<?php while (have_posts()) { the_post(); ?>
+
+							<article class="post-entry">
+								<?php get_template_part( $path, 'content' ); ?>
+							</article>
+
+						<?php } ?>
+
+					</div><!--/ .post-list-->
+
+				</section><!--/ .section-->
+			<?php
+		}
+
+		if ($pagination == 'yes') {
+			get_template_part('content', 'pagenavi');
+		}
+			$wp_query = $original_query;
+			wp_reset_postdata();
+
+	}
---
+++ views/shortcodes/interpress/contact_form.php
@@ -0,0 +1,166 @@
+<?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
+<?php
+//contact form on front by shortcode
+$form_name = $content;
+$contact_form = TMM_Contact_Form::get_form($form_name);
+wp_enqueue_script("tmm_shortcode_contact_form_js", TMM_CC_URL . '/js/shortcodes/contact_form.js');
+$unique_id = uniqid();
+$form_type = (isset($contact_form['form_type'])) ? $contact_form['form_type'] : 'default';
+
+if (!isset($dont_fill_inputs)) {
+	$dont_fill_inputs = true;
+}
+
+//output fields
+if (!empty($contact_form['inputs'])) {
+	?>
+
+	<form method="post" class="contact-form" enctype="multipart/form-data">
+
+		<input type="hidden" name="contact_form_name" value="<?php echo $form_name ?>" />
+            
+            <?php if ($form_type!='default'){ ?>
+
+                <div class="<?php echo ($form_type=='type2') ? 'full-inputs-block' : 'inputs-block' ?>">
+                
+            <?php } ?>
+
+                <?php
+                foreach ($contact_form['inputs'] as $key => $input){
+
+	                $name = $input['type'] . $key;
+
+                    switch ($input['type']) {
+                        case "email":
+                            ?>
+                            <p class="input-block type-input">
+                                <input id="email_<?php echo $unique_id ?>" <?php echo($input['is_required'] ? "required" : "") ?> type="email" name="<?php echo $name ?>" value="<?php echo(!empty($_POST) ? $dont_fill_inputs ? "" : $_POST[$name]  : "") ?>" placeholder="<?php echo $input['label'] ?><?php echo($input['is_required'] ? " *" : "") ?>" />
+                            </p>
+                            <?php
+                            break;
+
+                        case "textinput":
+                            ?>
+                            <p class="input-block type-input">
+                                <input id="name_<?php echo $unique_id ?>" <?php echo($input['is_required'] ? "required" : "") ?> type="text" name="<?php echo $name ?>" value="<?php echo(!empty($_POST) ? $dont_fill_inputs ? "" : $_POST[$name]  : "") ?>" placeholder="<?php echo $input['label'] ?><?php echo($input['is_required'] ? " *" : "") ?>" />
+                            </p>
+                            <?php
+                            break;
+
+
+                        case "checkbox":
+                            ?>
+                            <p class="input-block">						
+                                <input type="checkbox" name="<?php echo $name ?>" value="0" class="contact_form_option_checkbox" /><?php echo $input['label'] ?>						
+                            </p>
+                            <?php
+                            break;
+
+                        case "radio":
+                            $radio_items = explode(",", $input['options']);
+                            if (!empty($radio_items)):
+                                ?>
+                                <p class="input-block">
+
+                                    <?php foreach ($radio_items as $item) : ?>
+                                        <label>
+                                            <input type="radio" name="<?php echo $name ?>" value="<?php echo $item ?>" />&nbsp;<?php echo $item ?>
+                                        </label>
+                                    <?php endforeach; ?>
+
+                                </p>
+                                <?php
+                            endif;
+                            break;
+
+                        case "website":
+                            ?>
+                            <p class="input-block type-input">
+                                <input id="url_<?php echo $unique_id ?>" <?php echo($input['is_required'] ? "required" : "") ?> type="url" name="<?php echo $name ?>" value="<?php echo(!empty($_POST) ? $dont_fill_inputs ? "" : $_POST[$name]  : "") ?>" placeholder="<?php echo $input['label'] ?><?php echo($input['is_required'] ? " *" : "") ?>" />
+                            </p>
+                            <?php
+                            break;				
+
+                        case "select":
+                            $select_options = explode(",", $input['options']);
+                            ?>
+                            <p class="input-block sel">
+	                            <label for="url_<?php echo $unique_id ?>"><?php echo $input['label'] ?><?php echo($input['is_required'] ? ': <span class="required">(' . __('required', TMM_CC_TEXTDOMAIN) . ')</span>' : '') ?></label>
+	                            <select id="url_<?php echo $unique_id ?>" name="<?php echo $name ?>">
+		                            <?php if (!empty($select_options)): ?>
+                                        <?php foreach ($select_options as $value) : ?>
+                                            <option value="<?php echo $value; ?>"><?php echo $value; ?></option>
+                                        <?php endforeach; ?>
+                                    <?php endif; ?>
+                                </select>
+                            </p>
+                            <?php
+                            break;
+                        default:
+                            break;
+                    }
+                } ?>
+
+                <?php if ($form_type!='default'){ ?>
+
+                </div><!--/ .inputs-block-->
+                
+                <div class="<?php echo ($form_type=='type2') ? 'full-textarea-block' : 'textarea-block' ?>">
+                    
+                <?php } ?>
+                    
+                    <?php foreach ($contact_form['inputs'] as $key => $input){
+
+	                    $name = $input['type'] . $key;
+
+                        if ($input['type']=='messagebody'){
+                            ?>
+                            <?php
+
+                            if (empty($name)) {
+                                    $name = "messagebody";
+                            }
+                            ?>
+                                <p class="input-block">
+                                        <textarea id="message_<?php echo $unique_id ?>" <?php echo($input['is_required'] ? "required" : "") ?> name="<?php echo $name ?>" placeholder="<?php echo $input['label'] ?><?php echo($input['is_required'] ? " *" : "") ?>" ><?php echo(!empty($_POST) ? $dont_fill_inputs ? "" : $_POST[$name]  : "") ?></textarea>
+                                </p>
+                            <?php
+                        }
+			
+                    }
+                    ?>
+                    <?php if ($contact_form['has_capture']){ ?>
+
+                        <p class="input-block">
+                            <?php $hash = md5(time()); ?>
+                            <img class="contact_form_capcha" src="<?php echo get_stylesheet_directory_uri(); ?>/helper/capcha/image.php?hash=<?php echo $hash ?>" height="27" width="72" /><input type="text" value="" name="verify" class="verify" /><input type="hidden" name="verify_code" value="<?php echo $hash ?>" />
+                        </p><!--/ .row-->
+
+                    <?php } ?>
+                    <p>
+                        <?php if ($form_type=='type2'){ ?>
+                            <button class="button middle <?php echo $contact_form['submit_button'] ?>" type="submit"><?php echo ($contact_form['submit_button_text']) ? $contact_form['submit_button_text'] : '<i class="icon-paper-plane-2"></i>'?></button>
+                            <?php
+                        }else{
+                            ?>
+                            <button class="button submit <?php echo $contact_form['submit_button'] ?>" type="submit"><?php echo ($contact_form['submit_button_text']) ? $contact_form['submit_button_text'] : '<i class="icon-paper-plane-2"></i>'?></button>
+                            <?php
+                        } ?>
+			
+                    </p>
+                
+                <?php if ($form_type!='default'){ ?>
+                    
+                </div><!--/ .textarea-block-->
+                
+                <?php } ?>		
+                                
+		<div class="contact_form_responce" style="display: none;"><ul></ul></div>
+	</form>
+
+
+	<?php
+}
+?>
+<div class="clear"></div>
+
