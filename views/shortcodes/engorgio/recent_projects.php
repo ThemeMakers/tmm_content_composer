ffe985a026d4423e65c1a48b3b21b217804ea2f0
Julia Samoilenko <jullsum88@gmail.com>
2015-08-31 13:22:23 +0300

    recent_projects and folio update  (html validation)

--- views/shortcodes/engorgio/recent_projects.php
+++ views/shortcodes/engorgio/recent_projects.php
@@ -19,17 +19,6 @@ $posts = get_posts($args);
             <div class="item-overlay">
                 <div class="extra-content">
                     <h2 class="extra-title"><?php echo esc_html($post->post_title); ?></h2>
-                    <h6 class="extra-category">
-                        <?php
-                            $tags = wp_get_post_tags($post->ID);
-                            foreach ($tags as $key => $value) {
-                                if ($key > 0) {
-                                        echo ' / ';
-                                }
-                                echo $value->name;
-                            }
-                        ?>
-                    </h6>
 
                     <a class="single-image plus-icon popup-link" href="<?php echo (get_post_thumbnail_id($post->ID)) ? esc_url(TMM_Content_Composer::get_post_featured_image($post->ID, '')) : ''; ?>"><?php _e('Image', TMM_CC_TEXTDOMAIN) ?></a>
                     <a class="single-image link-icon" href="<?php echo esc_url(get_permalink($post->ID)); ?>"><?php _e('Permalink', TMM_CC_TEXTDOMAIN) ?></a>
