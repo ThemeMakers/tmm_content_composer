4b7297dc0b7fb4dbcf440c37beaa0f3ff73684b1
Oleksiy Ostapenko <lelik4w@gmail.com>
2015-11-11 16:37:30 +0100

    update blockquote shortcode

--- views/shortcodes/default/blockquote.php
+++ views/shortcodes/default/blockquote.php
@@ -1,2 +1,25 @@
 <?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
-<blockquote> <?php echo $content ?></blockquote>
+
+<?php
+
+// Html
+
+$css_class = "";
+
+if (!isset($positioning)) {
+	$positioning = 'fullwidth';
+}
+
+if ($positioning == "left") {
+	$css_class .= 'class="quote-left"';
+} elseif ($positioning == "right") {
+	$css_class .= 'class="quote-right"';
+} else {
+	$css_class .= '';
+}
+
+?>
+
+<blockquote <?php echo $css_class ?>>
+	<p><?php echo $content ?></p>
+</blockquote>
--- views/shortcodes/default/popups/blockquote.php
+++ views/shortcodes/default/popups/blockquote.php
@@ -1,6 +1,24 @@
 <?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
 <div id="tmm_shortcode_template" class="tmm_shortcode_template clearfix">
 
+	<div class="fullwidth">
+		<?php
+		TMM_Content_Composer::html_option(array(
+				'type' => 'select',
+				'title' => __('Quote Position', TMM_CC_TEXTDOMAIN),
+				'shortcode_field' => 'positioning',
+				'id' => 'positioning',
+				'options' => array(
+						'fullwidth' => __('Full Width', TMM_CC_TEXTDOMAIN),
+						'left' => __('Align Left', TMM_CC_TEXTDOMAIN),
+						'right' => __('Align Right', TMM_CC_TEXTDOMAIN),
+				),
+				'default_value' => TMM_Content_Composer::set_default_value('positioning', 'fullwidth'),
+				'description' => ''
+		));
+		?>
+	</div><!--/ .fullwidth-->
+
     <div class="fullwidth">
 
 		<?php
