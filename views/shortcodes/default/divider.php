e8555cdd9b36408363a28137008c8025b0ffe8f0
Oleksiy Ostapenko <lelik4w@gmail.com>
2015-11-13 22:02:45 +0100

    update divider shortcode; update services shortcode style

--- views/shortcodes/default/divider.php
+++ views/shortcodes/default/divider.php
@@ -1,23 +1,20 @@
 <?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
+
+
 <?php
-$style = $content;
-switch ($style) {
-	case 'separator':
-		?>
-		<div class="clear"></div>
-		<div class="divider"></div>
-		<?php
-		break;
-	case 'empty';
-		?>
-		<div class="clear"></div>
-		<div class="white-space"></div>
-		<?php
-		break;
-	default:
-		?>
-		<div class="clear"></div>
-		<div class="divider"></div>
-		<?php
-		break;
+
+if (!isset($type)) {
+	$type = 'type-1';
 }
+
+if (!isset($size)) {
+	$size = 'middle';
+}
+
+if (!isset($color)) {
+	$color = 'gray';
+}
+
+?>
+
+<div class="lc-divider <?php echo $type . ' ' . $size . ' ' . $color ?>"></div><!-- /.lc-divider -->
