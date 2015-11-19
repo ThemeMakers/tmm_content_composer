0dddf7f6ae85924c46002b15fa41c1eff3ee21b5
Oleksiy Ostapenko <lelik4w@gmail.com>
2015-11-20 00:03:42 +0100

    car-531 Add Captions to images shortcode on hover

--- views/shortcodes/default/image.php
+++ views/shortcodes/default/image.php
@@ -2,24 +2,61 @@
 <?php
 
 $image_url = $content;
+$css_class = "";
+$css_caption_class = "";
 $styles = "";
 $html = "";
-	
-// Margins
-if (!empty($margin_left))   { $styles .= 'margin-left: ' . (int) $margin_left . 'px; '; }
-if (!empty($margin_right))  { $styles .= 'margin-right: ' . (int) $margin_right . 'px; '; }
-if (!empty($margin_top))    { $styles .= 'margin-top: ' . (int) $margin_top . 'px; '; }
-if (!empty($margin_bottom)) { $styles .= 'margin-bottom: ' . (int) $margin_bottom . 'px; '; }
+$figcaption = "";
 
 // Styles
 if (!empty($styles)) {
-	$styles = 'style="' . $styles . '"';
+	$styles = ' style="' . $styles . '"';
+}
+
+// Align
+if (!empty($align))         { $css_class .= ' ' . $align; }
+
+// Inner offset
+if (!empty($inner_offset))  { $css_class .= ' ' . $inner_offset; }
+
+// Outer offsets
+if (!empty($margin_top))    { $css_class .= ' ' . $margin_top; }
+if (!empty($margin_right))  { $css_class .= ' ' . $margin_right; }
+if (!empty($margin_bottom)) { $css_class .= ' ' . $margin_bottom; }
+if (!empty($margin_left))   { $css_class .= ' ' . $margin_left; }
+
+// Border style
+if (!empty($brd_style))     { $css_class .= ' ' . $brd_style; }
+
+// Border width
+if (!empty($brd_width))     { $css_class .= ' ' . $brd_width; }
+
+// Border color
+if (!empty($brd_color))     { $css_class .= ' ' . $brd_color; }
+
+// Hover effects
+if (!empty($hover_effect))  { $css_class .= ' ' . $hover_effect; }
+
+// Caption Type
+if (!empty($caption_type))  { $css_caption_class .= $caption_type; }
+
+// Caption Style
+if (!empty($caption_style))  { $css_caption_class .= ' ' . $caption_style; }
+
+// Caption Classes
+if (!empty($css_caption_class)) {
+	$css_caption_class = ' class="' . $css_caption_class . '"';
+}
+
+// Caption
+if (!empty($img_caption)) {
+	$figcaption .= '<figcaption' . $css_caption_class . '>' . $img_caption . '</figcaption>';
 }
 
 // Fancybox
 if ($fancybox) {
 	$src = TMM_Helper::resize_image($image_url, '');
-	$image_action_link = $src;
+	$target_url = $src;
 	$link_class = 'fancybox';
 } else {
 	$src = TMM_Helper::resize_image($image_url, $image_size_alias);
@@ -27,10 +64,10 @@ if ($fancybox) {
 }
 
 if ($action == "link") {
-	$html.= '<a title="' . $link_title . '" class="single-image ' . $link_class . '" ' . $styles . '  href="' . $image_action_link . '" target="' . $target . '">';
+	$html.= '<a title="' . $img_caption . '" class="single-image ' . $link_class . '" href="' . $target_url . '" target="' . $target . '">';
 }
 
-$html.= '<img class="' . $align . '" alt="' . $image_alt . '" '. ($action == "link" ? '' : $styles) .' src="' . $src . '" />';
+$html.= '<figure class="lc-image' . $css_class . '"><img alt="' . $image_alt . '" src="' . $src . '" />' . $figcaption . '</figure>';
 
 if ($action == "link") { 
 	$html .= '</a>'; 
--- views/shortcodes/default/popups/image.php
+++ views/shortcodes/default/popups/image.php
@@ -13,68 +13,194 @@
 		));
 		?>
 
-	</div><!--/ .one-half-->
+		<div class="row">
+			<div class="one-half">
+				<?php
+				TMM_Content_Composer::html_option(array(
+					'type' => 'select',
+					'title' => __('Image Hover Effect', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'hover_effect',
+					'id' => 'hover_effect',
+					'options' => array(
+						'' => __('None', TMM_CC_TEXTDOMAIN),
+						'opacity' => __('Opacity', TMM_CC_TEXTDOMAIN),
+						'grayscale' => __('Grayscale', TMM_CC_TEXTDOMAIN),
+						'sepia' => __('Sepia', TMM_CC_TEXTDOMAIN),
+						'saturate' => __('Saturate', TMM_CC_TEXTDOMAIN),
+						'brightness' => __('Brightness', TMM_CC_TEXTDOMAIN),
+						'contrast' => __('Contrast', TMM_CC_TEXTDOMAIN),
+						'blur' => __('Blur', TMM_CC_TEXTDOMAIN),
+					),
+					'default_value' => TMM_Content_Composer::set_default_value('hover_effect', ''),
+					'description' => ''
+				));
+				?>
+			</div>
+			<div class="one-half">
+				<?php
+				$action = TMM_Content_Composer::set_default_value('action', 'none');
+				//***
+				TMM_Content_Composer::html_option(array(
+					'type' => 'select',
+					'title' => __('Action', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'action',
+					'id' => 'img_shortcode_action',
+					'options' => array(
+						'none' => __('No link on image', TMM_CC_TEXTDOMAIN),
+						'link' => __('Open link', TMM_CC_TEXTDOMAIN),
+					),
+					'default_value' => $action,
+					'description' => ''
+				));
+				?>
 
+			</div>
+		</div>
 
-	<div class="one-half">
+		<div id="target_url" style="display: <?php echo($action == 'none' ? 'none' : 'block') ?>;">
 
-		<?php
-		$action = TMM_Content_Composer::set_default_value('action', 'none');
-		//***
-		TMM_Content_Composer::html_option(array(
-			'type' => 'select',
-			'title' => __('Action', TMM_CC_TEXTDOMAIN),
-			'shortcode_field' => 'action',
-			'id' => 'img_shortcode_action',
-			'options' => array(
-				'none' => __('No link on image', TMM_CC_TEXTDOMAIN),
-				'link' => __('Open link', TMM_CC_TEXTDOMAIN),
-			),
-			'default_value' => $action,
-			'description' => ''
-		));
-		?>
-
-		<div id="image_action_link" style="display: <?php echo($action == 'none' ? 'none' : 'block') ?>;">
-			<?php
-			TMM_Content_Composer::html_option(array(
-				'type' => 'text',
-				'title' => __('Image Action Link', TMM_CC_TEXTDOMAIN),
-				'shortcode_field' => 'image_action_link',
-				'id' => 'image_action_link',
-				'default_value' => TMM_Content_Composer::set_default_value('image_action_link', '#'),
-				'description' => ''
-			));
-			?>
+			<div class="row">
+				<div class="one-half">
+					<?php
+					TMM_Content_Composer::html_option(array(
+						'type' => 'text',
+						'title' => __('Target URL', TMM_CC_TEXTDOMAIN),
+						'shortcode_field' => 'target_url',
+						'id' => 'target_url',
+						'default_value' => TMM_Content_Composer::set_default_value('target_url', '#'),
+						'description' => ''
+					));
+					?>
+				</div>
+				<div class="one-half">
+					<?php
+					TMM_Content_Composer::html_option(array(
+						'type' => 'select',
+						'title' => __('Link Target', TMM_CC_TEXTDOMAIN),
+						'shortcode_field' => 'target',
+						'id' => 'target',
+						'options' => array(
+							'_self' => __('Self', TMM_CC_TEXTDOMAIN),
+							'_blank' => __('Blank', TMM_CC_TEXTDOMAIN),
+						),
+						'default_value' => TMM_Content_Composer::set_default_value('target', '_self'),
+						'description' => ''
+					));
+					?>
+				</div>
+			</div>
 
 			<br />
 
 			<?php
 			TMM_Content_Composer::html_option(array(
-				'type' => 'select',
-				'title' => __('Link Target', TMM_CC_TEXTDOMAIN),
-				'shortcode_field' => 'target',
-				'id' => 'target',
-				'options' => array(
-					'_self' => __('Self', TMM_CC_TEXTDOMAIN),
-					'_blank' => __('Blank', TMM_CC_TEXTDOMAIN),
-				),
-				'default_value' => TMM_Content_Composer::set_default_value('target', '_self'),
-				'description' => ''
+					'type' => 'checkbox',
+					'title' => __('Open original image size in Fancybox', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'fancybox',
+					'id' => 'fancybox',
+					'is_checked' => TMM_Content_Composer::set_default_value('fancybox', 0),
+					'description' => ''
 			));
 			?>
-			
-			<?php
-			TMM_Content_Composer::html_option(array(
-				'type' => 'checkbox',
-				'title' => __('Open in Fancybox', TMM_CC_TEXTDOMAIN),
-				'shortcode_field' => 'fancybox',
-				'id' => 'fancybox',
-				'is_checked' => TMM_Content_Composer::set_default_value('fancybox', 0),
-				'description' => ''
-			));
-			?>	
 
+		</div>
+
+		<div class="row">
+			<div class="one-half">
+				<?php
+				TMM_Content_Composer::html_option(array(
+					'type' => 'select',
+					'title' => __('Align', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'align',
+					'id' => 'align',
+					'options' => array(
+						'' => __('None', TMM_CC_TEXTDOMAIN),
+						'alignleft' => __('Left', TMM_CC_TEXTDOMAIN),
+						'alignright' => __('Right', TMM_CC_TEXTDOMAIN),
+						'aligncenter' => __('Center', TMM_CC_TEXTDOMAIN),
+					),
+					'default_value' => TMM_Content_Composer::set_default_value('align', ''),
+					'description' => ''
+				));
+				?>
+			</div>
+			<div class="one-half">
+				<?php
+				TMM_Content_Composer::html_option(array(
+					'type' => 'select',
+					'title' => __('Size', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'image_size_alias',
+					'id' => 'image_size_alias',
+					'options' => TMM_OptionsHelper::get_theme_image_sizes_aliases(),
+					'default_value' => TMM_Content_Composer::set_default_value('image_size_alias', ''),
+					'description' => ''
+				));
+				?>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="one-half">
+				<?php
+				TMM_Content_Composer::html_option(array(
+					'type' => 'select',
+					'title' => __('Caption Type', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'caption_type',
+					'id' => 'caption_type',
+					'options' => array(
+						'' => __('Default (Beneath Image)', TMM_CC_TEXTDOMAIN),
+						'static' => __('Static (Over Image)', TMM_CC_TEXTDOMAIN),
+						'animated' => __('Animated (Over Image)', TMM_CC_TEXTDOMAIN),
+					),
+					'default_value' => TMM_Content_Composer::set_default_value('caption_type', ''),
+					'description' => ''
+				));
+				?>
+			</div>
+			<div class="one-half">
+				<?php
+				TMM_Content_Composer::html_option(array(
+					'type' => 'select',
+					'title' => __('Caption Style', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'caption_style',
+					'id' => 'caption_style',
+					'options' => array(
+						'' => __('Default', TMM_CC_TEXTDOMAIN),
+						'style-1' => __('Light Translucent BG', TMM_CC_TEXTDOMAIN),
+						'style-2' => __('Dark Translucent BG', TMM_CC_TEXTDOMAIN),
+					),
+					'default_value' => TMM_Content_Composer::set_default_value('caption_style', ''),
+					'description' => ''
+				));
+				?>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="one-half">
+				<?php
+				TMM_Content_Composer::html_option(array(
+					'type' => 'text',
+					'title' => __('Image Alt', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'image_alt',
+					'id' => 'image_alt',
+					'default_value' => TMM_Content_Composer::set_default_value('image_alt', ''),
+					'description' => ''
+				));
+				?>
+			</div>
+			<div class="one-half">
+				<?php
+				TMM_Content_Composer::html_option(array(
+					'type' => 'text',
+					'title' => __('Image Caption', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'img_caption',
+					'id' => 'img_caption',
+					'default_value' => TMM_Content_Composer::set_default_value('img_caption', ''),
+					'description' => ''
+				));
+				?>
+			</div>
 		</div>
 
 	</div><!--/ .one-half-->
@@ -84,116 +210,169 @@
 		<?php
 		TMM_Content_Composer::html_option(array(
 			'type' => 'select',
-			'title' => __('Align', TMM_CC_TEXTDOMAIN),
-			'shortcode_field' => 'align',
-			'id' => 'align',
+			'title' => __('Inner Offset', TMM_CC_TEXTDOMAIN),
+			'shortcode_field' => 'inner_offset',
+			'id' => 'inner_offset',
 			'options' => array(
 				'' => __('None', TMM_CC_TEXTDOMAIN),
-				'alignleft' => __('Left', TMM_CC_TEXTDOMAIN),
-				'alignright' => __('Right', TMM_CC_TEXTDOMAIN),
-				'aligncenter' => __('Center', TMM_CC_TEXTDOMAIN),
+				'io-1' => __('Smaller...........1px', TMM_CC_TEXTDOMAIN),
+				'io-3' => __('Small..............3px', TMM_CC_TEXTDOMAIN),
+				'io-5' => __('Middle............5px', TMM_CC_TEXTDOMAIN),
+				'io-10' => __('Large.............10px', TMM_CC_TEXTDOMAIN),
 			),
-			'default_value' => TMM_Content_Composer::set_default_value('align', ''),
+			'default_value' => TMM_Content_Composer::set_default_value('inner_offset', ''),
 			'description' => ''
 		));
 		?>
 
-	</div><!--/ .one-half-->
+		<div class="row">
+			<div class="one-half">
 
-	<div class="one-half">
+				<?php
+				TMM_Content_Composer::html_option(array(
+					'type' => 'select',
+					'title' => __('Outer Offset (top)', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'margin_top',
+					'id' => 'margin_top',
+					'options' => array(
+						'' => __('None', TMM_CC_TEXTDOMAIN),
+						'oo-t-5' => __('Smaller...........5px', TMM_CC_TEXTDOMAIN),
+						'oo-t-10' => __('Small..............10px', TMM_CC_TEXTDOMAIN),
+						'oo-t-15' => __('Middle............15px', TMM_CC_TEXTDOMAIN),
+						'oo-t-20' => __('Large.............20px', TMM_CC_TEXTDOMAIN),
+					),
+					'default_value' => TMM_Content_Composer::set_default_value('margin_top', ''),
+					'description' => ''
+				));
+				?>
+
+			</div><!--/ .one-half-->
+
+			<div class="one-half">
+
+				<?php
+				TMM_Content_Composer::html_option(array(
+					'type' => 'select',
+					'title' => __('Outer Offset (right)', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'margin_right',
+					'id' => 'margin_right',
+					'options' => array(
+						'' => __('None', TMM_CC_TEXTDOMAIN),
+						'oo-r-5' => __('Smaller...........5px', TMM_CC_TEXTDOMAIN),
+						'oo-r-10' => __('Small..............10px', TMM_CC_TEXTDOMAIN),
+						'oo-r-15' => __('Middle............15px', TMM_CC_TEXTDOMAIN),
+						'oo-r-20' => __('Large.............20px', TMM_CC_TEXTDOMAIN),
+					),
+					'default_value' => TMM_Content_Composer::set_default_value('margin_right', ''),
+					'description' => ''
+				));
+				?>
+
+			</div><!--/ .one-half-->
+		</div>
+
+		<div class="row">
+			<div class="one-half">
+
+				<?php
+				TMM_Content_Composer::html_option(array(
+					'type' => 'select',
+					'title' => __('Outer Offset (bottom)', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'margin_bottom',
+					'id' => 'margin_bottom',
+					'options' => array(
+						'' => __('None', TMM_CC_TEXTDOMAIN),
+						'oo-b-5' => __('Smaller...........5px', TMM_CC_TEXTDOMAIN),
+						'oo-b-10' => __('Small..............10px', TMM_CC_TEXTDOMAIN),
+						'oo-b-15' => __('Middle............15px', TMM_CC_TEXTDOMAIN),
+						'oo-b-20' => __('Large.............20px', TMM_CC_TEXTDOMAIN),
+					),
+					'default_value' => TMM_Content_Composer::set_default_value('margin_bottom', ''),
+					'description' => ''
+				));
+				?>
+
+			</div><!--/ .one-half-->
+
+			<div class="one-half">
+
+				<?php
+				TMM_Content_Composer::html_option(array(
+					'type' => 'select',
+					'title' => __('Outer Offset (left)', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'margin_left',
+					'id' => 'margin_left',
+					'options' => array(
+						'' => __('None', TMM_CC_TEXTDOMAIN),
+						'oo-l-5' => __('Smaller...........5px', TMM_CC_TEXTDOMAIN),
+						'oo-l-10' => __('Small..............10px', TMM_CC_TEXTDOMAIN),
+						'oo-l-15' => __('Middle............15px', TMM_CC_TEXTDOMAIN),
+						'oo-l-20' => __('Large.............20px', TMM_CC_TEXTDOMAIN),
+					),
+					'default_value' => TMM_Content_Composer::set_default_value('margin_left', ''),
+					'description' => ''
+				));
+				?>
+
+			</div><!--/ .one-half-->
+		</div>
+
+		<div class="row">
+			<div class="one-half">
+
+				<?php
+				TMM_Content_Composer::html_option(array(
+					'type' => 'select',
+					'title' => __('Border Style', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'brd_style',
+					'id' => 'brd_style',
+					'options' => array(
+						'brd-solid' => __('Solid', TMM_CC_TEXTDOMAIN),
+						'brd-dashed' => __('Dashed', TMM_CC_TEXTDOMAIN),
+						'brd-dotted' => __('Dotted', TMM_CC_TEXTDOMAIN),
+					),
+					'default_value' => TMM_Content_Composer::set_default_value('brd_style', 'brd-solid'),
+					'description' => ''
+				));
+				?>
+
+			</div><!--/ .one-half-->
+
+			<div class="one-half">
+
+				<?php
+				TMM_Content_Composer::html_option(array(
+					'type' => 'select',
+					'title' => __('Border Width', TMM_CC_TEXTDOMAIN),
+					'shortcode_field' => 'brd_width',
+					'id' => 'brd_width',
+					'options' => array(
+						'brd-w-0' => __('None', TMM_CC_TEXTDOMAIN),
+						'brd-w-1' => __('Small..............1px', TMM_CC_TEXTDOMAIN),
+						'brd-w-2' => __('Middle............2px', TMM_CC_TEXTDOMAIN),
+					),
+					'default_value' => TMM_Content_Composer::set_default_value('brd_width', 'brd-w-0'),
+					'description' => ''
+				));
+				?>
+
+			</div><!--/ .one-half-->
+		</div>
 
 		<?php
 		TMM_Content_Composer::html_option(array(
 			'type' => 'select',
-			'title' => __('Size', TMM_CC_TEXTDOMAIN),
-			'shortcode_field' => 'image_size_alias',
-			'id' => 'image_size_alias',
-			'options' => TMM_OptionsHelper::get_theme_image_sizes_aliases(),
-			'default_value' => TMM_Content_Composer::set_default_value('image_size_alias', ''),
-			'description' => ''
-		));
-		?>
-
-	</div><!--/ .one-half-->
-
-	<div class="one-half">
-		<?php
-		TMM_Content_Composer::html_option(array(
-			'type' => 'text',
-			'title' => __('Image Alt', TMM_CC_TEXTDOMAIN),
-			'shortcode_field' => 'image_alt',
-			'id' => 'image_alt',
-			'default_value' => TMM_Content_Composer::set_default_value('image_alt', ''),
-			'description' => ''
-		));
-		?>
-
-	</div><!--/ .one-half-->
-
-	<div class="one-half">
-		<?php
-		TMM_Content_Composer::html_option(array(
-			'type' => 'text',
-			'title' => __('Link Title', TMM_CC_TEXTDOMAIN),
-			'shortcode_field' => 'link_title',
-			'id' => 'link_title',
-			'default_value' => TMM_Content_Composer::set_default_value('link_title', ''),
-			'description' => ''
-		));
-		?>
-
-	</div><!--/ .one-half-->
-
-	<div class="one-half">
-		<?php
-		TMM_Content_Composer::html_option(array(
-			'type' => 'text',
-			'title' => __('Top Indent (px)', TMM_CC_TEXTDOMAIN),
-			'shortcode_field' => 'margin_top',
-			'id' => 'margin_top',
-			'default_value' => TMM_Content_Composer::set_default_value('margin_top', ''),
-			'description' => ''
-		));
-		?>
-
-	</div><!--/ .one-half-->
-
-	<div class="one-half">
-		<?php
-		TMM_Content_Composer::html_option(array(
-			'type' => 'text',
-			'title' => __('Right Indent (px)', TMM_CC_TEXTDOMAIN),
-			'shortcode_field' => 'margin_right',
-			'id' => 'margin_right',
-			'default_value' => TMM_Content_Composer::set_default_value('margin_right', ''),
-			'description' => ''
-		));
-		?>
-
-	</div><!--/ .one-half-->
-
-	<div class="one-half">
-		<?php
-		TMM_Content_Composer::html_option(array(
-			'type' => 'text',
-			'title' => __('Bottom Indent (px)', TMM_CC_TEXTDOMAIN),
-			'shortcode_field' => 'margin_bottom',
-			'id' => 'margin_bottom',
-			'default_value' => TMM_Content_Composer::set_default_value('margin_bottom', ''),
-			'description' => ''
-		));
-		?>
-
-	</div><!--/ .one-half-->
-
-	<div class="one-half">
-		<?php
-		TMM_Content_Composer::html_option(array(
-			'type' => 'text',
-			'title' => __('Left Indent (px)', TMM_CC_TEXTDOMAIN),
-			'shortcode_field' => 'margin_left',
-			'id' => 'margin_left',
-			'default_value' => TMM_Content_Composer::set_default_value('margin_left', ''),
+			'title' => __('Border Color', TMM_CC_TEXTDOMAIN),
+			'shortcode_field' => 'brd_color',
+			'id' => 'brd_color',
+			'options' => array(
+				'brd-light-grey' => __('Light Grey', TMM_CC_TEXTDOMAIN),
+				'brd-grey' => __('Grey', TMM_CC_TEXTDOMAIN),
+				'brd-white' => __('White', TMM_CC_TEXTDOMAIN),
+				'brd-white-transparent' => __('White with opacity', TMM_CC_TEXTDOMAIN),
+				'brd-grey-transparent' => __('Grey with opacity', TMM_CC_TEXTDOMAIN),
+			),
+			'default_value' => TMM_Content_Composer::set_default_value('brd_color', 'brd-light-grey'),
 			'description' => ''
 		));
 		?>
@@ -215,9 +394,9 @@
 		});
 
 		jQuery('#img_shortcode_action').on('change', function() {
-			jQuery("#image_action_link").hide(300);
+			jQuery("#target_url").hide(300);
 			if (jQuery(this).val() == 'link') {
-				jQuery("#image_action_link").show(300);
+				jQuery("#target_url").show(300);
 			}
 		});
 		
