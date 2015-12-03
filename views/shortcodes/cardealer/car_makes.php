30da57fa2904a203f7407525e9aa6f687b4296f9
Stanislav Samoilenko <st.prog.1986@gmail.com>
2015-12-03 10:31:42 +0200

    fixed strange code

--- views/shortcodes/cardealer/car_makes.php
+++ views/shortcodes/cardealer/car_makes.php
@@ -22,8 +22,9 @@ $args = array(
 
 $makes = get_terms('carproducer', $args);
 
-$show_name === '0';
-
+if (!isset($show_name)) {
+	$show_name = 1;
+}
 ?>
 
 <ul class="carproducers_list">
@@ -48,7 +49,7 @@ $show_name === '0';
 		if($make->count > 0 || !$hide_empty){
 			?>
 
-			<li class="cat-item-<?php echo $make->term_id; ?><?php if($show_name === '1'){ ?> with-title<?php } ?>">
+			<li class="cat-item-<?php echo $make->term_id; ?><?php if($show_name){ ?> with-title<?php } ?>">
 
 			<?php if (!isset($show_link) || $show_link) { ?>
 				<a title="<?php echo sprintf(__('View all ads filed under %s', TMM_CC_TEXTDOMAIN), $make->name); ?>" href="<?php echo get_term_link($make->slug, 'carproducer'); ?>">
@@ -58,16 +59,16 @@ $show_name === '0';
 					<img src="<?php echo $src; ?>" alt="<?php echo $make->name; ?>" />
 				<?php } ?>
 
-				<?php if($show_name === '1'){ ?>
+				<?php if($show_name){ ?>
 					<span class="car-title">
 				<?php } ?>
 
 					<?php
-					echo (!isset($show_name) || $show_name) ? $make->name : '';
+					echo ($show_name) ? $make->name : '';
 					echo (!isset($show_count) || $show_count) ? ' (' . $make->count . ')&#x200E;' : '';
 					?>
 
-				<?php if($show_name === '1'){ ?>
+				<?php if($show_name){ ?>
 					</span>
 				<?php } ?>
 
