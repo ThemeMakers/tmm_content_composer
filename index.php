26a024517713531a8b965f63cecf0d1cc153fe12
Stanislav Samoilenko <st.prog.1986@gmail.com>
2015-12-09 12:34:37 +0200

    text domain fix

--- index.php
+++ index.php
@@ -20,8 +20,6 @@ require_once TMM_CC_DIR.'classes/shortcode.php';
 /* Register */
 function tmm_cc_register() {
 
-	load_plugin_textdomain( 'tmm_content_composer', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-
 	TMM_Content_Composer::get_instance();
 
 	if ( !function_exists('tmm_enqueue_script') ) {
@@ -51,3 +49,12 @@ function tmm_cc_register() {
 }
 
 add_action( 'init', 'tmm_cc_register' );
+
+/**
+ * Load plugin textdomain.
+ */
+function tmm_cc_load_textdomain() {
+	load_plugin_textdomain( 'tmm_content_composer', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+}
+
+add_action( 'plugins_loaded', 'tmm_cc_load_textdomain' );
