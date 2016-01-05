515eed5673677e1272adbbba718e9981c074095a
Julia Samoilenko <jullsum88@gmail.com>
2016-01-05 16:04:38 +0200

    int-64 Update divider shortcode

--- views/shortcodes/default/divider.php
+++ views/shortcodes/default/divider.php
@@ -1,29 +1,20 @@
 <?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
+
+
 <?php
-$style = $content;
-switch ($style) {
-	case 'separator':
-		?>
-		<div class="clear"></div>
-		<div class="separator"></div>
-		<?php
-		break;
-	case 'divider':
-		?>
-		<div class="clear"></div>
-		<div class="divider"></div>
-		<?php
-		break;
-	case 'double-divider';
-		?>
-		<div class="clear"></div>
-		<div class="double-divider"></div>
-		<?php
-		break;
-	default:
-		?>
-		<div class="clear"></div>
-		<div class="divider"></div>
-		<?php
-		break;
+
+if (!isset($type)) {
+	$type = 'type-1';
 }
+
+if (!isset($size)) {
+	$size = 'middle';
+}
+
+if (!isset($color)) {
+	$color = 'gray';
+}
+
+?>
+
+<div class="lc-divider <?php echo $type . ' ' . $size . ' ' . $color ?>"></div><!-- /.lc-divider -->
--- views/shortcodes/default/popups/divider.php
+++ views/shortcodes/default/popups/divider.php
@@ -6,14 +6,50 @@
 		TMM_Content_Composer::html_option(array(
 			'type' => 'select',
 			'title' => __('Select Type', 'tmm_content_composer'),
-			'shortcode_field' => 'content',
-			'id' => '',
+			'shortcode_field' => 'type',
+			'id' => 'type',
 			'options' => array(
-				'separator' => __('Line', 'tmm_content_composer'),
-				'divider' => __('Dashed', 'tmm_content_composer'),
-				'double-divider' => __('Double Divider', 'tmm_shortcodes'),
+				'type-1' => __('Solid', 'tmm_content_composer'),
+				'type-2' => __('Solid Short', 'tmm_content_composer'),
+				'type-3' => __('Empty Space', 'tmm_content_composer'),
 			),
-			'default_value' => TMM_Content_Composer::set_default_value('content', 'separator'),
+			'default_value' => TMM_Content_Composer::set_default_value('type', 'type-1'),
+			'description' => ''
+		));
+		?>
+
+		<?php
+		TMM_Content_Composer::html_option(array(
+			'type' => 'select',
+			'title' => __('Space around divider', 'tmm_content_composer'),
+			'shortcode_field' => 'size',
+			'id' => 'size',
+			'options' => array(
+				'small' => __('Small', 'tmm_content_composer'),
+				'middle' => __('Middle', 'tmm_content_composer'),
+				'large' => __('Large', 'tmm_content_composer'),
+			),
+			'default_value' => TMM_Content_Composer::set_default_value('size', 'middle'),
+			'description' => ''
+		));
+		?>
+
+	</div><!--/ .one-half-->
+
+	<div class="one-half">
+
+		<?php
+		TMM_Content_Composer::html_option(array(
+			'type' => 'select',
+			'title' => __('Buttons Color', 'tmm_content_composer'),
+			'shortcode_field' => 'color',
+			'id' => 'color',
+			'options' => array(
+				'transparent' => __('Transparent', 'tmm_content_composer'),
+				'gray' => __('Gray', 'tmm_content_composer'),
+				'dark' => __('Dark', 'tmm_content_composer'),
+			),
+			'default_value' => TMM_Content_Composer::set_default_value('color', 'gray'),
 			'description' => ''
 		));
 		?>
@@ -29,10 +65,9 @@
 
 	jQuery(function() {
 		tmm_ext_shortcodes.changer(shortcode_name);
-		jQuery("#tmm_shortcode_template .js_shortcode_template_changer").on('keyup change', function() {
+		jQuery("#tmm_shortcode_template .js_shortcode_template_changer").on('click change keyup', function() {
 			tmm_ext_shortcodes.changer(shortcode_name);
 		});
+
 	});
 </script>
-
-
