fefc7b5d4f85ada0f2208ba37432eeeec4e3a32f
Julia Samoilenko <jullsum88@gmail.com>
2016-01-05 13:27:08 +0200

    int-59 Fix progressbar shortcode functionality

--- views/shortcodes/default/progressbar.php
+++ views/shortcodes/default/progressbar.php
@@ -4,19 +4,21 @@ $titles = explode('^', $title);
 $colors = explode('^', $color);
 $percentages = explode('^', $percentage);
 ?>
-	
-<?php if (!empty($colors)): ?>
-	<?php foreach ($colors as $key => $color) : ?>
 
-		<div class="progress-bar">
-			<div class="progressbar-title-wrap">
-				<div class="progressbar-title"><?php echo esc_html($titles[$key]) ?></div>
-			</div><!--/ .progressbar-title-wrap-->
-			<div class="bar-outer">
-				<div data-progress="<?php echo esc_attr($percentages[$key]) ?>" class="bar" style="background-color: <?php echo $color; ?>"></div>
-			</div><!--/ .bar-outer-->
-			<span class="percent">0%</span>
-		</div><!--/ .progress-bar-->
-
-	<?php endforeach; ?>
-<?php endif; ?>	
+<ul class="progress-list-shortcode">
+	<?php if (!empty($colors)){ ?>
+		<?php foreach ($colors as $key => $color) { ?>
+			<li>
+				<div class="progress-bar">
+					<div class="progress-title">
+						<div class="progressbar-title"><?php echo esc_html($titles[$key]) ?></div>
+					</div><!--/ .progressbar-title-wrap-->
+					<div class="progress-bar-outer">
+						<div data-progress="<?php echo esc_attr($percentages[$key]/10) ?>" class="bar" style="background-color: <?php echo $color; ?>"></div>
+					</div><!--/ .bar-outer-->
+					<span class="progress-percent"><?php echo $percentages[$key]; ?>%</span>
+				</div><!--/ .progress-bar-->
+			</li>
+			<?php } ?>
+	<?php } ?>
+</ul>
