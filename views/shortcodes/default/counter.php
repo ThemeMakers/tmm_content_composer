7214e5ebb2e79d5a5a0b54bccc534074199e057d
Oleksiy Ostapenko <lelik4w@gmail.com>
2016-01-10 17:50:55 +0100

    update counter shortcode styles

--- views/shortcodes/default/counter.php
+++ views/shortcodes/default/counter.php
@@ -5,16 +5,16 @@ $from = explode('^', $from);
 $to = explode('^', $to);
 ?>
 <?php if (!empty($titles)): ?>
-	<div class="counter-box">
+	<ul class="lc-counter-box">
 
 		<?php foreach ($titles as $key => $title) : ?>
-			<div data-from="<?php echo esc_attr($from[$key]) ?>" data-to="<?php echo esc_attr($to[$key]) ?>" data-speed="<?php echo esc_attr($speed) ?>" class="counter <?php if ($animation) echo esc_attr($animation) ?>">
+			<li data-from="<?php echo esc_attr($from[$key]) ?>" data-to="<?php echo esc_attr($to[$key]) ?>" data-speed="<?php echo esc_attr($speed) ?>" class="counter <?php if ($animation) echo esc_attr($animation) ?>">
 				<span class="count"></span>
 				<h4 class="details"><?php echo esc_html($title) ?></h4>
-			</div><!--/ .counter-->	
+			</li><!--/ .counter-->
 		<?php endforeach; ?>
 			
-	</div><!--/ .counter-box-->	
+	</ul><!--/ .counter-box-->
 <?php endif; ?>
 
 
