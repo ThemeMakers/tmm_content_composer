165b8a4f5f8727cf05a010915dae08c1513e6863
Julia Samoilenko <jullsum88@gmail.com>
2016-01-21 10:25:15 +0200

    int-76 fix pricing table shortcode

--- views/shortcodes/default/pricing_tables.php
+++ views/shortcodes/default/pricing_tables.php
@@ -1,9 +1,11 @@
 <?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
 <section class="lc-pricing-table col-<?php echo $count ?>">
 	<?php
-	$content = str_replace('__PRICE_TABLE__', '[price_table effect_type='.$effect_type, $content);
+	$content = str_replace('__PRICE_TABLE__', '[price_table effect_type="'.$effect_type.'"', $content);
 	$content = str_replace('__PRICE_TABLE_CLOSE__', ']', $content);
 	$content = str_replace('__PRICE_TABLE_END__', '[/price_table]', $content);
+	$content = str_replace('&#8221;', '"', $content);
+	$content = str_replace('&#8243;', '"', $content);
 	echo do_shortcode(str_replace('<br />', '', $content));
 	?>
 </section>
