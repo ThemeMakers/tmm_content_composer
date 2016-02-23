ab355f1d7ba7762d04d5789405991a28fb392a16
Julia Samoilenko <jullsum88@gmail.com>
2016-02-23 16:01:45 +0200

    int-90 Update Authors shortcode

--- views/shortcodes/interpress/authors.php
+++ views/shortcodes/interpress/authors.php
@@ -28,6 +28,12 @@ if ($userid=='all'){
 								<?php echo stripslashes($user->description); ?>
 							</p>
 
+							<?php if (!empty($user->user_url)){ ?>
+								<p>
+									<a href="<?php echo esc_url($user->user_url); ?>" target="_blank"><?php echo $user->user_url; ?></a>
+								</p>
+							<?php } ?>
+
 							<?php TMM_USERS::my_author_social_links($user->ID); ?>
 
 						</div><!--/ .author-description-->
@@ -44,32 +50,92 @@ if ($userid=='all'){
 
 } else {
 
-	$user = get_userdata($userid); ?>
+	$user = get_userdata($userid);
 
-		<?php if (is_object($user)){ ?>
+	?>
 
-			<ul class="lc-authors-list">
+	<?php if (is_object($user)) { ?>
 
-				<li>
+		<ul class="lc-authors-list">
 
-					<div class="author-avatar">
-						<?php echo get_avatar($user->user_email, 130); ?>
-					</div><!--/ .author-avatar-->
+			<li>
 
-					<div class="author-description">
-						<h5><?php echo $user->display_name ?></h5>
+				<div class="author-avatar">
+					<?php echo get_avatar($user->user_email, 130); ?>
+				</div>
+				<!--/ .author-avatar-->
 
+				<div class="author-description">
+					<h5><?php echo $user->display_name ?></h5>
+
+					<p>
+						<?php echo stripslashes($user->description); ?>
+					</p>
+
+					<?php if (!empty($user->user_url)) { ?>
 						<p>
-							<?php echo stripslashes($user->description); ?>
+							<a href="<?php echo esc_url($user->user_url); ?>"
+							   target="_blank"><?php echo $user->user_url; ?></a>
 						</p>
+					<?php } ?>
 
-						<?php TMM_USERS::my_author_social_links($userid); ?>
+					<?php TMM_USERS::my_author_social_links($userid); ?>
 
-					</div><!--/ .author-description-->
+				</div>
+				<!--/ .author-description-->
 
-				</li>
-			</ul>
+			</li>
+		</ul>
 
-		<?php } ?>
+	<?php } ?>
+
+	<?php if (isset($display_posts) && $display_posts) {
+		?>
+		<section class="section padding-off">
+
+			<?php
+			global $post;
+			$path = $path = 'content/blog-medium/content';
+			$user_query = array(
+				'orderby' => $orderby,
+				'order' => $order,
+				'post_status' => array('publish'),
+				'posts_per_page' => 3,
+				'author' => $userid
+			);
+
+			$user_query['posts_per_page'] = (!empty($posts_per_page)) ? $posts_per_page : '-1';
+			$user_query['paged'] = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+			global $wp_query;
+			$original_query = $wp_query;
+			$wp_query = new WP_Query($user_query);
+			?>
+
+			<?php if (have_posts()) { ?>
+
+				<div class="post-list">
+
+					<?php while (have_posts()) {
+						the_post(); ?>
+
+						<article class="post-entry">
+							<?php get_template_part($path, 'content'); ?>
+						</article>
+
+					<?php } ?>
+
+				</div><!--/ .post-list-->
+
+			<?php } ?>
+
+		</section><!--/ .section-->
+
 	<?php
+		if ($pagination == 'yes') {
+			get_template_part('content', 'pagenavi');
+		}
+		$wp_query = $original_query;
+		wp_reset_postdata();
+	}
 }
--- views/shortcodes/interpress/popups/authors.php
+++ views/shortcodes/interpress/popups/authors.php
@@ -25,6 +25,62 @@
 	</div>
 
 	<div class="one-half option-author">
+
+		<?php
+		TMM_Content_Composer::html_option(array(
+			'type' => 'checkbox',
+			'title' => __("Display author's posts", 'tmm_content_composer'),
+			'shortcode_field' => 'display_posts',
+			'id' => 'display_posts',
+			'is_checked' => TMM_Content_Composer::set_default_value('display_posts', 1),
+			'description' => __("Display author's posts", 'tmm_content_composer')
+		));
+		?>
+
+	</div>
+
+	<div class="one-half option-posts">
+
+		<?php
+		TMM_Content_Composer::html_option(array(
+			'type' => 'select',
+			'title' => __('Sort By', 'tmm_content_composer'),
+			'shortcode_field' => 'orderby',
+			'id' => 'orderby',
+			'options' => array(
+				'ID' => __('ID', 'tmm_content_composer'),
+				'author' => __('Author', 'tmm_content_composer'),
+				'title' => __('Title', 'tmm_content_composer'),
+				'date' => __('Date', 'tmm_content_composer'),
+				'modified' => __('Modified', 'tmm_content_composer'),
+				'type' => __('Post Type', 'tmm_content_composer'),
+				'comment_count' => __('Comment Count', 'tmm_content_composer'),
+			),
+			'default_value' => TMM_Content_Composer::set_default_value('orderby', ''),
+			'description' => ''
+		));
+		?>
+
+	</div><!--/ .ona-half-->
+
+	<div class="one-half option-posts">
+		<?php
+		TMM_Content_Composer::html_option(array(
+			'type' => 'select',
+			'title' => __('Sort', 'tmm_content_composer'),
+			'shortcode_field' => 'order',
+			'id' => 'order',
+			'options' => array(
+				'DESC' => __('DESC', 'tmm_content_composer'),
+				'ASC' => __('ASC', 'tmm_content_composer'),
+			),
+			'default_value' => TMM_Content_Composer::set_default_value('order', 'DESC'),
+			'description' => ''
+		));
+		?>
+	</div><!--/ .ona-half-->
+
+	<div class="one-half option-posts">
 	<?php
 		TMM_Content_Composer::html_option(array(
 			'type' => 'text',
@@ -37,7 +93,7 @@
 	?>
 	</div>
 
-	<div class="one-half option-author">
+	<div class="one-half option-posts">
 	<?php
 		TMM_Content_Composer::html_option(array(
 			'type' => 'select',
@@ -64,23 +120,44 @@
 		});
 
 		var option_author = jQuery('.option-author'),
+			option_posts = jQuery('.option-posts'),
 			type = jQuery('#user_id'),
+			display_posts = jQuery('#display_posts'),
+			dpVal = display_posts.val(),
 			typeVal = type.val();
 
+		switchPosts(dpVal);
 		switchAuthorType(typeVal);
+
+		display_posts.on('change', function(){
+			var val = jQuery(this).val();
+			switchPosts(val);
+		});
 
 		type.on('change', function(){
 			var val = jQuery(this).val();
 			switchAuthorType(val);
 		});
 
+		function switchPosts(val){
+			if (val=='1'){
+				option_posts.slideDown();
+			}else{
+				option_posts.slideUp();
+			}
+		}
+
 		function switchAuthorType(val){
 			switch(val){
 				case 'all':
 					option_author.slideUp();
+					option_posts.slideUp();
 					break;
 				default:
 					option_author.slideDown();
+					if (jQuery('#display_posts').val() == '1'){
+						option_posts.slideDown();
+					}
 					break;
 			}
 		}
