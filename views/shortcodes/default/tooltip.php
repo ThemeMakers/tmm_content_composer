50fcb3ae5d938a2ca722da8ec3e71177c7cbdc12
Stanislav Samoilenko <st.prog.1986@gmail.com>
2016-02-24 12:13:44 +0200

    fixed tooltip shortcode

--- views/shortcodes/default/tooltip.php
+++ views/shortcodes/default/tooltip.php
@@ -1,10 +1,2 @@
-<?php
-if (!defined('ABSPATH')) exit;
-
-wp_enqueue_style('tmm_tooltipster');
-wp_enqueue_script('tmm_tooltipster');
-?>
-<a href="<?php echo $link ?>" target="<?php echo $link_target ?>"
-   <?php if (!empty($tooltip)): ?> class="highlight tooltip tooltipstered" title="<?php echo $tooltip ?>" <?php endif; ?>>
-   <?php echo $content ?>
-</a>
+<?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
+<a href="<?php echo $link ?>" target="<?php echo $link_target ?>" <?php if (!empty($tooltip)): ?> class="tooltip" title="<?php echo $tooltip ?>" <?php endif; ?>><?php echo $content ?></a>
