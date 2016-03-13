6a19bcfad6be1bd5ead64458fd150114d6a62e50
Oleksiy Ostapenko <lelik4w@gmail.com>
2016-03-13 21:45:34 +0100

    update plugin version; update plugin's translation files

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Description: Universal Layout Composer with Shortcodes Package
  * Author: ThemeMakers
  * Author URI: http://themeforest.net/user/ThemeMakers
- * Version: 1.2.12
+ * Version: 1.2.13
  * Text Domain: tmm_content_composer
 */
 
