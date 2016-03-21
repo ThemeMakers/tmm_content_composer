bf84cefd0d393937f4e7c41c7fe4fefcee4c8098
Stanislav Samoilenko <st.prog.1986@gmail.com>
2016-03-21 12:02:09 +0200

    int-96 added fields to vCard shortcode

--- views/shortcodes/default/popups/vCard.php
+++ views/shortcodes/default/popups/vCard.php
@@ -1,18 +1,77 @@
 <?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
 <div id="tmm_shortcode_template" class="tmm_shortcode_template clearfix">
 
-    <div class="fullwidth">
+    <div class="one-half">
+
+		<?php
+		TMM_Content_Composer::html_option(array(
+			'type' => 'text',
+			'title' => __('Organization Name', 'tmm_content_composer'),
+			'shortcode_field' => 'name',
+			'id' => '',
+			'default_value' => TMM_Content_Composer::set_default_value('name', ''),
+			'description' => ''
+		));
+		?>
 
 		<?php
 		TMM_Content_Composer::html_option(array(
 			'type' => 'text',
 			'title' => __('Enter Address', 'tmm_content_composer'),
-			'shortcode_field' => 'content',
+			'shortcode_field' => 'address',
 			'id' => '',
-			'default_value' => TMM_Content_Composer::set_default_value('content', ''),
+			'default_value' => TMM_Content_Composer::set_default_value('address', ''),
 			'description' => ''
 		));
 		?>
+
+		<?php
+		TMM_Content_Composer::html_option(array(
+			'type' => 'text',
+			'title' => __('Enter City', 'tmm_content_composer'),
+			'shortcode_field' => 'city',
+			'id' => '',
+			'default_value' => TMM_Content_Composer::set_default_value('city', ''),
+			'description' => ''
+		));
+		?>
+
+		<?php
+		TMM_Content_Composer::html_option(array(
+			'type' => 'text',
+			'title' => __('Enter Region', 'tmm_content_composer'),
+			'shortcode_field' => 'region',
+			'id' => '',
+			'default_value' => TMM_Content_Composer::set_default_value('region', ''),
+			'description' => ''
+		));
+		?>
+
+		<?php
+		TMM_Content_Composer::html_option(array(
+			'type' => 'text',
+			'title' => __('Enter Postal Code', 'tmm_content_composer'),
+			'shortcode_field' => 'postal_code',
+			'id' => '',
+			'default_value' => TMM_Content_Composer::set_default_value('postal_code', ''),
+			'description' => ''
+		));
+		?>
+
+		<?php
+		TMM_Content_Composer::html_option(array(
+			'type' => 'text',
+			'title' => __('Enter Country', 'tmm_content_composer'),
+			'shortcode_field' => 'country',
+			'id' => '',
+			'default_value' => TMM_Content_Composer::set_default_value('country', ''),
+			'description' => ''
+		));
+		?>
+
+    </div>
+
+    <div class="one-half">
 
 		<?php
 		TMM_Content_Composer::html_option(array(
@@ -47,6 +106,27 @@
 		));
 		?>
 
+		<?php
+		TMM_Content_Composer::html_option(array(
+			'type' => 'checkbox',
+			'title' => __('Show Labels', 'tmm_content_composer'),
+			'shortcode_field' => 'labels',
+			'id' => 'tmm-labels',
+			'is_checked' => TMM_Content_Composer::set_default_value('labels', 1),
+			'description' => ''
+		));
+		?>
+
+		<?php
+		TMM_Content_Composer::html_option(array(
+			'type' => 'checkbox',
+			'title' => __('Show Icons', 'tmm_content_composer'),
+			'shortcode_field' => 'icons',
+			'id' => 'tmm-icons',
+			'is_checked' => TMM_Content_Composer::set_default_value('icons', 1),
+			'description' => ''
+		));
+		?>
 
     </div><!--/ .fullwidth-->
 
--- views/shortcodes/default/vCard.php
+++ views/shortcodes/default/vCard.php
@@ -1,17 +1,65 @@
 <?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
 <ul class="our-contacts">
-    <?php if (!empty($content)): ?>
-            <li class="address"><b><?php _e('Address Info', 'tmm_content_composer') ?>:</b> <?php echo $content ?></li>
+
+    <?php if (!empty($name)): ?>
+        <li class="name<?php echo !empty($icons) ? ' icon-sitemap' : ''; ?>">
+            <?php if (!empty($labels)): ?>
+            <b><?php _e('Organization Name', 'tmm_content_composer') ?>:</b>
+            <?php endif; ?>
+	        <?php echo esc_html($name) ?>
+        </li>
+    <?php endif; ?>
+
+	<?php if (!empty($address) || !empty($city) || !empty($region)): ?>
+            <li class="address<?php echo !empty($icons) ? ' icon-address' : ''; ?>">
+	            <?php if (!empty($labels)): ?>
+	            <b><?php _e('Address Info', 'tmm_content_composer') ?>:</b>
+	            <?php endif; ?>
+	            <?php if (!empty($address)): ?>
+		            <span class="street-address"><?php echo esc_html($address) ?></span>,
+	            <?php endif; ?>
+
+	            <?php if (!empty($city)): ?>
+		            <span class="locality"><?php echo esc_html($city); ?></span>,<br>
+	            <?php endif; ?>
+
+	            <?php if (!empty($region)): ?>
+		            <span class="region"><?php echo esc_html($region); ?></span>&nbsp;
+	            <?php endif; ?>
+
+	            <?php if (!empty($postal_code)): ?>
+		            <span class="postal-code"><?php echo esc_html($postal_code); ?></span>&nbsp;
+	            <?php endif; ?>
+
+	            <?php if (!empty($country)): ?>
+		            <span class="country"><?php echo esc_html($country); ?></span>
+	            <?php endif; ?>
+            </li>
     <?php endif; ?>
     <?php if (!empty($phone)): ?>
-            <li class="phone"><b><?php _e('Phone', 'tmm_content_composer') ?>:</b> <?php echo $phone ?> </li>
+            <li class="phone<?php echo !empty($icons) ? ' icon-phone' : ''; ?>">
+                <?php if (!empty($labels)): ?>
+	            <b><?php _e('Phone', 'tmm_content_composer') ?>:</b>
+                <?php endif; ?>
+	            <?php echo esc_html($phone) ?>
+            </li>
     <?php endif; ?>
 			
 	<?php if (!empty($fax)): ?>
-		<li class="fax"><b><?php _e('FAX', 'tmm_content_composer') ?>:</b> <?php echo $fax ?></li>
+		<li class="fax<?php echo !empty($icons) ? ' icon-print' : ''; ?>">
+			<?php if (!empty($labels)): ?>
+			<b><?php _e('FAX', 'tmm_content_composer') ?>:</b>
+			<?php endif; ?>
+			<?php echo esc_html($fax) ?>
+		</li>
 	<?php endif; ?>   
 
     <?php if (!empty($email)): ?>
-            <li class="email"><b><?php _e('Email', 'tmm_content_composer') ?>:</b> <a href="mailto:<?php echo $email ?>"><?php echo $email ?></a></li>
+            <li class="email<?php echo !empty($icons) ? ' icon-email' : ''; ?>">
+	            <?php if (!empty($labels)): ?>
+	            <b><?php _e('Email', 'tmm_content_composer') ?>:</b>
+	            <?php endif; ?>
+	            <a href="mailto:<?php echo esc_attr($email) ?>"><?php echo esc_html($email) ?></a>
+            </li>
     <?php endif; ?>
 </ul>
