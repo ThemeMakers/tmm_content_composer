cb9d10432ce115c172ad15b64c691f04db479838
Stanislav Samoilenko <st.prog.1986@gmail.com>
2016-04-27 19:40:46 +0300

    car-661 Issue with the captcha

--- views/shortcodes/default/contact_form.php
+++ views/shortcodes/default/contact_form.php
@@ -101,7 +101,7 @@ if (!empty($contact_form['inputs'])) {
 			<p class="input-block">
 				<label><?php _e('Are you human?', TMM_CC_TEXTDOMAIN); ?></label>
 				<?php $hash = md5(time()); ?>
-				<img class="contact_form_capcha" src="<?php echo get_stylesheet_directory_uri(); ?>/helper/capcha/image.php?hash=<?php echo $hash ?>" height="29" width="72" alt="" />
+				<img class="contact_form_capcha" src="<?php echo get_template_directory_uri(); ?>/helper/capcha/image.php?hash=<?php echo $hash ?>" height="29" width="72" alt="" />
 				<input type="text" value="" name="verify" class="verify" />
 				<input type="hidden" name="verify_code" value="<?php echo $hash ?>" />
 			</p><!--/ .row-->
