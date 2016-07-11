196886f2c430e28566a738990dce4fcf35af296e
Oleksiy Ostapenko <lelik4w@gmail.com>
2016-07-11 22:58:27 +0200

    update translation files

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Description: Universal Layout Composer with Shortcodes Package
  * Author: ThemeMakers
  * Author URI: http://themeforest.net/user/ThemeMakers
- * Version: 1.2.16
+ * Version: 1.3.0
  * Text Domain: tmm_content_composer
 */
 
