4517cd887fb0c246863107e831a3ac04a753cc25
Dmitry Serg <dimonserg@ukr.net>
2016-09-27 15:05:50 +0300

    Google Maps API key changes added

--- views/shortcodes/default/google_map.php
+++ views/shortcodes/default/google_map.php
@@ -1,84 +1,97 @@
-<?php
+<?php if (!defined('ABSPATH')) die('No direct access allowed');
 
-$inique_id = uniqid();
-$google_maps_api_key = (isset($key)) ? 'key=' . $key . '&' : '' ;
-$map_link = '//maps.google.com/maps/api/js?' . $google_maps_api_key . 'sensor=false';
-wp_enqueue_script("tmm_google_api", $map_link);
+if (TMM::get_option("api_key_google")){
 
-$js_controls = '{}';
+	$inique_id = uniqid();
 
-if (!isset($mode)) {
-	$mode = 'map';
-}
+	$google_maps_api_key = (TMM::get_option("api_key_google")) ? 'key=' . TMM::get_option("api_key_google") . '&' : '' ;
+	$map_link = '//maps.google.com/maps/api/js?' . $google_maps_api_key . 'sensor=false';
+	wp_enqueue_script("tmm_shortcode_google_api_js", $map_link);
 
-if (isset($location_mode) && $location_mode == 'address') {
-	$address = str_replace(' ', '+', $address);
-	$geocode = @file_get_contents('http://maps.google.com/maps/api/geocode/json?address=' . $address . '&sensor=false');
-	if($geocode){
-		$output = json_decode($geocode);
-		if (isset($output->status) && $output->status != 'OVER_QUERY_LIMIT') {
-			$latitude = $output->results[0]->geometry->location->lat;
-			$longitude = $output->results[0]->geometry->location->lng;
+	$js_controls = '{}';
+
+	if (!isset($mode)) {
+		$mode = 'map';
+	}
+
+	if (isset($location_mode) && $location_mode == 'address') {
+		$address = str_replace(' ', '+', $address);
+		$geocode = @file_get_contents('http://maps.google.com/maps/api/geocode/json?address=' . $address . '&sensor=false');
+		if($geocode){
+			$output = json_decode($geocode);
+			if (isset($output->status) && $output->status != 'OVER_QUERY_LIMIT') {
+				$latitude = $output->results[0]->geometry->location->lat;
+				$longitude = $output->results[0]->geometry->location->lng;
+			} else {
+				$maptype = 'image';
+			}
+		}
+	}
+
+
+	if (!isset($maptype)) {
+		$maptype = 'image';
+	}
+
+	$slide_up = (isset($slide_up) && $slide_up) ? true : false;
+
+	if (isset($latitude) && $latitude !== '' && isset($longitude) && $longitude !== '') {
+
+		if ($mode == 'map') {
+	        if ($slide_up){
+	            $height = $height + 260;
+			?>
+	            <div class="google_map_expand">
+	                <div id="map_extended" class="google_map_toggle" data-height="<?php echo $height-260; ?>" style="height: <?php echo $height-260 ?>px;">
+	        <?php } ?>
+	                    <div class="google_map" id="google_map_<?php echo $inique_id ?>" style="height: <?php echo $height ?>px;"></div>
+	        <?php if ($slide_up){ ?>
+	                </div>
+					<span class="google_map_close"></span>
+				</div>
+	        <?php } ?>
+			<script type="text/javascript">
+				jQuery(function() {
+					gmt_init_map(<?php echo esc_js($latitude) ?>,<?php echo esc_js($longitude) ?>, "google_map_<?php echo $inique_id ?>", <?php echo esc_js($zoom) ?>, "<?php echo esc_js($maptype) ?>", "<?php echo esc_js($content) ?>", "<?php echo $enable_marker ?>", "<?php echo $enable_popup ?>", "<?php echo $enable_scrollwheel ?>",<?php echo $js_controls ?>, "<?php echo @$marker_is_draggable ?>");
+				});
+			</script>
+
+		<?php
 		} else {
-			$maptype = 'image';
-		}
-	}
-}
 
+			$marker_string = '';
+			if ($enable_marker) {
+				$marker_string = '&markers=color:red|label:P|' . $latitude . ',' . $longitude;
+			}
 
-if (!isset($maptype)) {
-	$maptype = 'image';
-}
+			$location_mode_string = 'center=' . $latitude . ',' . $longitude;
 
-$slide_up = (isset($slide_up) && $slide_up) ? true : false;
+	         if ($slide_up){
+	            $height = $height + 260;
+			?>
+	            <div class="google_map_expand">
+	                <div id="map_extended" class="google_map_toggle" data-height="<?php echo $height-260; ?>" style="height: <?php echo $height-260 ?>px;">
+	        <?php } ?>
 
-if (isset($latitude) && $latitude !== '' && isset($longitude) && $longitude !== '') {
+	                <img src="http://maps.googleapis.com/maps/api/staticmap?<?php echo $location_mode_string ?>&zoom=<?php echo (int) $zoom ?>&maptype=<?php echo strtolower($maptype) ?>&size=<?php echo (int)$width ?>x<?php echo (int)$height ?><?php echo $marker_string ?>&key=<?php echo TMM::get_option("api_key_google")?>&sensor=false">
 
-	if ($mode == 'map') {
-        if ($slide_up){
-            $height = $height + 260;
-		?>
-            <div class="google_map_expand">
-                <div id="map_extended" class="google_map_toggle" data-height="<?php echo $height-260; ?>" style="height: <?php echo $height-260 ?>px;">
-        <?php } ?>             
-                    <div class="google_map" id="google_map_<?php echo $inique_id ?>" style="height: <?php echo $height ?>px;"></div>
-        <?php if ($slide_up){ ?>           
-                </div>
-				<span class="google_map_close"></span>
-			</div>		
-        <?php } ?>
-		<script type="text/javascript">
-			jQuery(function() {
-				gmt_init_map(<?php echo esc_js($latitude) ?>,<?php echo esc_js($longitude) ?>, "google_map_<?php echo $inique_id ?>", <?php echo esc_js($zoom) ?>, "<?php echo esc_js($maptype) ?>", "<?php echo esc_js($content) ?>", "<?php echo $enable_marker ?>", "<?php echo $enable_popup ?>", "<?php echo $enable_scrollwheel ?>",<?php echo $js_controls ?>, "<?php echo @$marker_is_draggable ?>");
-			});
-		</script>
-
-	<?php
-	} else {
-
-		$marker_string = '';
-		if ($enable_marker) {
-			$marker_string = '&markers=color:red|label:P|' . $latitude . ',' . $longitude;
+	        <?php if ($slide_up){ ?>
+	                </div>
+					<span class="google_map_close"></span>
+				</div>
+	        <?php } ?>
+		<?php
 		}
 
-		$location_mode_string = 'center=' . $latitude . ',' . $longitude;
-		
-         if ($slide_up){
-            $height = $height + 260;
-		?>
-            <div class="google_map_expand">
-                <div id="map_extended" class="google_map_toggle" data-height="<?php echo $height-260; ?>" style="height: <?php echo $height-260 ?>px;">
-        <?php } ?> 
-
-                <img src="http://maps.googleapis.com/maps/api/staticmap?<?php echo $location_mode_string ?>&zoom=<?php echo $zoom ?>&maptype=<?php echo strtolower($maptype) ?>&size=<?php echo $width ?>x<?php echo $height ?><?php echo $marker_string ?>&sensor=false">
-        
-        <?php if ($slide_up){ ?>           
-                </div>
-				<span class="google_map_close"></span>
-			</div>		
-        <?php } ?>
-	<?php
 	}
 
+
 }
+
+else{
+	echo "<h4>";
+	echo _e('Enter your Google Maps API key on Theme Options Page.', TMM_CC_TEXTDOMAIN);
+	echo "</h4>";
+}
+
 ?>
--- views/shortcodes/default/popups/google_map.php
+++ views/shortcodes/default/popups/google_map.php
@@ -3,22 +3,6 @@
 <div id="tmm_shortcode_template" class="tmm_shortcode_template clearfix">
 
     <div class="one-half">
-        <?php
-        TMM_Content_Composer::html_option(array(
-            'type' => 'text',
-            'title' => __('API Key', TMM_CC_TEXTDOMAIN),
-            'shortcode_field' => 'key',
-            'id' => 'key',
-            'default_value' => TMM_Content_Composer::set_default_value('key', ''),
-            'description' => '<a target="_blank" href="//forums.webtemplatemasters.com/getting-keys-for-google-maps/">Obtaining an API key</a>'
-        ));
-        ?>
-
-    </div><!--/ .one-half-->
-
-    <div class="one-half"></div><!--/ .one-half-->
-
-	<div class="one-half">
 		<?php
 		TMM_Content_Composer::html_option(array(
 			'type' => 'text',
@@ -94,7 +78,7 @@
 			'default_value' => TMM_Content_Composer::set_default_value('latitude', 40.714623),
 			'description' => __('Point on which the viewport will be centered. If not given and no markers are defined the viewport defaults to world view. In address Location mode it is calculated automatically!', TMM_CC_TEXTDOMAIN)
 		));
-		?>		
+		?>
 
 	</div><!--/ .one-half-->
 
@@ -149,7 +133,7 @@
 		?>
 
 	</div><!--/ .one-half-->
-    
+
     <div class="one-half">
 		<?php
 		TMM_Content_Composer::html_option(array(
@@ -160,7 +144,7 @@
 			'is_checked' => TMM_Content_Composer::set_default_value('slide_up', 1),
 			'description' => __('Show with Slide Up Effect', TMM_CC_TEXTDOMAIN)
 		));
-		?>		
+		?>
 	</div><!--/ .one-half-->
 
 	<div class="one-half">
@@ -192,7 +176,7 @@
 			'default_value' => TMM_Content_Composer::set_default_value('maptype', 'ROADMAP'),
 			'description' => ''
 		));
-		?>	
+		?>
 	</div><!--/ .one-half-->
 
 
@@ -234,7 +218,7 @@
 			'is_checked' => TMM_Content_Composer::set_default_value('marker_is_draggable', 0),
 			'description' => __('Set marker draggable', TMM_CC_TEXTDOMAIN)
 		));
-		?>		
+		?>
 	</div><!--/ .one-half-->
 
 	<div class="one-half">
@@ -248,7 +232,7 @@
 			'description' => ''
 		));
 		?>
-	</div><!--/ .one-half--> 
+	</div><!--/ .one-half-->
 
 </div>
 
