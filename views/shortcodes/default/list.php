23d601a883765844237c7912475598792ca95973
Oleksiy Ostapenko <lelik4w@gmail.com>
2017-05-28 23:05:35 +0200

    car-753 Issue with the fie malware

--- views/shortcodes/default/list.php
+++ views/shortcodes/default/list.php
@@ -6,7 +6,7 @@ $content = explode('^', $content);
 <<?php echo $list_type ?> class="list">
 <?php if (!empty($content)): ?>
 	<?php foreach ($content as $key => $text) : ?>
-		<li class="<?php echo $styles_array[$key] ?>"><b><?php echo $text ?></b></li>
+		<li class="<?php echo $styles_array[$key] ?>"><b><?php echo esc_html($text) ?></b></li>
 	<?php endforeach; ?>
 <?php endif; ?>
 </<?php echo $list_type ?>>
