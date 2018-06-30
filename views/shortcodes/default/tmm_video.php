153288ab3dd336c75630c5b7dadbfefa4993bfca
Oleksiy Ostapenko <lelik4w@gmail.com>
2018-06-30 16:29:32 +0200

    fix video/audio shortcodes

--- views/shortcodes/default/tmm_video.php
+++ views/shortcodes/default/tmm_video.php
@@ -1,24 +1,33 @@
 <?php if (!defined('ABSPATH')) die('No direct access allowed');
 
-switch ($type) {
-	case 'youtube':
+$allows_array = array('youtube.com', 'vimeo.com', '.mp4', '.ogv', '.webm');
+$video_type = '';
+foreach ($allows_array as $key => $needle) {
+	$count = strpos($content, $needle);
+	if ($count !== FALSE) {
+		$video_type = $allows_array[$key];
+	}
+}
+
+switch ($video_type) {
+	case $allows_array[0]:
 		$source_code = explode("?v=", $content);
 		$source_code = explode("&", $source_code[1]);
 		if (is_array($source_code)) {
 			$source_code = $source_code[0];
 		}
 		?>
-		<iframe allowtransparency="true" width="<?php echo esc_attr($width) ?>" height="<?php echo esc_attr($height) ?>" src="//www.youtube.com/embed/<?php echo esc_attr($source_code) ?>?wmode=transparent" allowfullscreen></iframe>
+		<div class="video-wrapper"><iframe allowtransparency="true" src="//www.youtube.com/embed/<?php echo esc_attr($source_code) ?>?wmode=transparent" allowfullscreen></iframe></div>
 		<?php
 		return "";
 		break;
-	case 'vimeo':
+	case $allows_array[1]:
 		$source_code = explode("/", $content);
 		if (is_array($source_code)) {
 			$source_code = $source_code[count($source_code) - 1];
 		}
 		?>
-		<iframe width="<?php echo esc_attr($width) ?>" height="<?php echo esc_attr($height) ?>" src="//player.vimeo.com/video/<?php echo esc_attr($source_code) ?>?title=0&amp;byline=0&amp;portrait=0&amp;color=f6e200"></iframe>
+		<div class="video-wrapper"><iframe src="//player.vimeo.com/video/<?php echo esc_attr($source_code) ?>?title=0&amp;byline=0&amp;portrait=0&amp;color=f6e200"></iframe></div>
 		<?php
 		break;
 
