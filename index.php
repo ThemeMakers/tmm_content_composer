e77b655af251f9aeb01fff57c210c48a2b45dd38
Oleksiy Ostapenko <lelik4w@gmail.com>
2018-11-04 15:40:14 +0100

    update plugin version

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Description: Universal Layout Composer with Shortcodes Package
  * Author: ThemeMakers
  * Author URI: http://themeforest.net/user/ThemeMakers
- * Version: 1.1.0
+ * Version: 1.1.1
  * Text Domain: tmm_content_composer
 */
 
