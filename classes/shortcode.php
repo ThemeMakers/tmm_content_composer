18794cfd073b2f0cb3b686cbd51696de69895e5f
Oleksiy Ostapenko <lelik4w@gmail.com>
2018-11-04 15:32:34 +0100

    quick fix shortcodes

--- classes/shortcode.php
+++ classes/shortcode.php
@@ -42,7 +42,17 @@ class TMM_Shortcode {
 		$shortcodes_keys = array_keys(self::$shortcodes);
 
 		function tmm_do_shortcode($atts, $content = '', $shortcode_key) {
+			if (is_array($atts) && isset($atts['content'])) {
+				$atts["content"] = $content;
+			}
+
+			if (!is_array($atts)) {
+				$atts = array();
+				$atts["content"] = $content;
+			}
+
 			$atts["content"] = $content;
+
 			if (isset($_REQUEST["shortcode_mode_edit"])) {
 				$_REQUEST["shortcode_mode_edit"] = $atts;
 			} else {
