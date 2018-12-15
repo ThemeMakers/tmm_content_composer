7708ab695530c0556e834042bebe88b877213b34
Oleksiy Ostapenko <lelik4w@gmail.com>
2018-12-15 02:57:35 +0100

    fix google map shortcode (the map was not displaying when lat/long fields were empty)

--- views/shortcodes/default/google_map.php
+++ views/shortcodes/default/google_map.php
@@ -2,114 +2,93 @@
 
 if (TMM::get_option("api_key_google")){
 
-$inique_id = uniqid();
-$google_maps_api_key = (TMM::get_option("api_key_google")) ? 'key=' . TMM::get_option("api_key_google") . '&' : '' ;
-$map_link = 'https://maps.google.com/maps/api/js?' . $google_maps_api_key . '&';
+    $inique_id = uniqid();
 
-wp_enqueue_script("tmm_cc_front");?>
-<script type='text/javascript' src='https://maps.google.com/maps/api/js?<?php echo esc_attr($google_maps_api_key) ?>&callback=initMap&ver=4.6.1' defer></script>
+    $google_maps_api_key = (TMM::get_option("api_key_google")) ? 'key=' . TMM::get_option("api_key_google") . '&' : '' ;
+    $map_link = 'https://maps.google.com/maps/api/js?' . $google_maps_api_key;
 
-<?php
+    $js_controls = '{}';
 
-if (!isset($mode)) {
-	$mode = 'map';
+    if (!isset($mode)) {
+        $mode = 'map';
+    }
+
+    $location_mode = isset($location_mode) ? $location_mode : '';
+
+    if ($location_mode === 'address') {
+        $address = str_replace(' ', '+', $address);
+        $geocode = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?address=' . $address . '&' . $google_maps_api_key);
+        $output = json_decode($geocode, true);
+
+        $latitude = '';
+        $longitude = '';
+
+        // if latitude & longitude does not defined by user
+        if ($output) {
+            if( $output['status'] == 'OK' ) {
+                $latitude = $output['results'][0]['geometry']['location']['lat'];
+                $longitude = $output['results'][0]['geometry']['location']['lng'];
+            } else {
+                printf( $output['error_message'] );
+            }
+        } else {
+            printf( 'GPS coordinates were not available because connection failed or malformed request' );
+        }
+    }
+
+    if (!isset($maptype)) {
+        $maptype = 'ROADMAP';
+    }
+
+    if (!isset($marker_is_draggable)) {
+        $marker_is_draggable = 0;
+    }
+
+    if ($mode === 'map') {
+
+        wp_enqueue_script("tmm_shortcode_google_api_js", $map_link);
+        wp_enqueue_script('tmm_composer_front');
+        ?>
+
+        <div class="google_map" id="google_map_<?php echo esc_attr($inique_id) ?>" style="height: <?php echo esc_attr($height) ?>px;"></div>
+
+        <script type="text/javascript">
+            jQuery(function() {
+                gmt_init_map(
+                    <?php echo esc_attr($latitude) ?>,
+                    <?php echo esc_attr($longitude) ?>,
+                    "google_map_<?php echo esc_attr($inique_id) ?>",
+                    <?php echo esc_attr($zoom) ?>,
+                    "<?php echo esc_attr($maptype) ?>",
+                    "<?php echo esc_attr($content) ?>",
+                    "<?php echo esc_attr($enable_marker) ?>",
+                    "<?php echo esc_attr($enable_popup) ?>",
+                    "<?php echo esc_attr($enable_scrollwheel) ?>",
+                    <?php echo esc_attr($js_controls) ?>,
+                    "<?php echo esc_attr($marker_is_draggable) ?>"
+                );
+            });
+        </script>
+    <?php } else { ?>
+        <?php
+        $marker_string = '';
+        if ($enable_marker) {
+            $marker_string = '&markers=color:red%7clabel:%7c' . $latitude . ',' . $longitude;
+        }
+
+        $location_mode_string = 'center=' . $latitude . ',' . $longitude;
+
+        $staticmap = 'https://maps.googleapis.com/maps/api/staticmap?' . $location_mode_string . '&zoom=' . (int) $zoom . '&maptype=' . strtolower($maptype) . '&size=' . (int)$width . 'x' . (int)$height . $marker_string . '&' . $google_maps_api_key;
+
+        ?>
+
+        <img src="https://maps.googleapis.com/maps/api/staticmap?<?php echo esc_attr($location_mode_string) ?>&zoom=<?php echo esc_attr($zoom) ?>&maptype=<?php echo strtolower($maptype) ?>&size=<?php echo esc_attr($width) ?>x<?php echo esc_attr($height) ?><?php echo esc_attr($marker_string) ?>&<?php echo esc_attr( $google_maps_api_key ) ?>">
+
+    <?php }
+
+} else {
+    $full_width = ($width == '' || $width == '100%') ? '1130' : $width;
+    $custom_height = ($height == '') ? '400' : $height;
+    $link_url = 'https://placeholdit.imgix.net/~text?txtsize=40&txt=Please+Enter+a+Valid+Google+API+key&w='. $full_width . '&h=' . $custom_height;
+    echo '<img class="aligncenter" src=' . $link_url . '>';
 }
-
-
-$controls = ""; //not need
-$js_controls = '{';
-if (!empty($controls)) {
-	$controls = explode(',', $controls);
-	if (!empty($controls)) {
-		foreach ($controls as $key => $value) {
-			if ($key > 0) {
-				$js_controls.=',';
-			}
-			$js_controls.=$value . ': true';
-		}
-	}
-}
-$js_controls.='}';
-?>
-
-<?php
-if ($location_mode == 'address') {
-	// if location does not defined by user
-	if('' === $address & '' === $latitude & '' === $longitude) {
-		$address = 'New York';
-	}
-
-	$address = str_replace(' ', '+', $address);
-	$geocode = file_get_contents('https://maps.google.com/maps/api/geocode/json?address=' . $address . '&' . $google_maps_api_key);
-	$output = json_decode($geocode, true);
-
-	// if latitude & longitude does not defined by user
-	if ('' === $latitude & '' === $longitude) {
-		if ($output) {
-			if( $output['status'] == 'OK' ) {
-				$latitude = $output['results'][0]['geometry']['location']['lat'];
-				$longitude = $output['results'][0]['geometry']['location']['lng'];
-			} else {
-				printf( $output['error_message'] );
-			}
-		} else {
-			printf( 'GPS coordinates were not available because connection failed or malformed request' );
-		}
-	}
-}
-?>
-
-<?php if ($mode == 'map'): ?>
-
-	<div class="google_map"
-		 id="google_map_<?php echo esc_attr($inique_id) ?>"
-		 style="height: <?php echo esc_attr( $height ) ?> px;" data-latitude="<?php echo esc_attr($latitude) ?>"
-         data-longitude="<?php echo esc_attr($longitude) ?>"
-         data-inique_id="<?php echo esc_attr($inique_id) ?>"
-         data-zoom="<?php echo esc_attr($zoom) ?>" data-maptype="<?php echo esc_attr($maptype) ?>"
-         data-content="<?php echo esc_attr($content) ?>"
-         data-enable_marker="<?php echo esc_attr($enable_marker) ?>"
-         data-enable_popup="<?php echo esc_attr($enable_popup) ?>"
-         data-enable_scrollwheel="<?php echo esc_attr($enable_scrollwheel) ?>"
-         data-js_controls="<?php echo esc_attr($js_controls) ?>"
-         data-marker_is_draggable="<?php echo esc_attr($marker_is_draggable) ?>"></div>
-
-	<script type="text/javascript">
-		var map;
-		function initMap() {
-			gmt_init_map(<?php echo esc_attr($latitude) ?>,
-				<?php echo esc_attr($longitude) ?>,
-				"google_map_<?php echo esc_attr($inique_id) ?>",
-				<?php echo esc_attr($zoom) ?>,
-				"<?php echo esc_attr($maptype) ?>",
-				"<?php echo esc_attr($content) ?>",
-				"<?php echo esc_attr($enable_marker) ?>",
-				"<?php echo esc_attr($enable_popup) ?>",
-				"<?php echo esc_attr($enable_scrollwheel) ?>",
-				<?php echo esc_attr($js_controls) ?>,
-				"<?php echo esc_attr($marker_is_draggable) ?>"
-			);
-		}
-	</script>
-<?php else: ?>
-	<?php
-	$marker_string = '';
-	if ($enable_marker) {
-		$marker_string = '&markers=color:red|label:P|' . $latitude . ',' . $longitude;
-	}
-
-	$location_mode_string = 'center=' . $latitude . ',' . $longitude;
-	?>
-
-	<img src="https://maps.googleapis.com/maps/api/staticmap?<?php echo esc_attr($location_mode_string) ?>&zoom=<?php echo esc_attr($zoom) ?>&maptype=<?php echo strtolower($maptype) ?>&size=<?php echo esc_attr($width) ?>x<?php echo esc_attr($height) ?><?php echo esc_attr($marker_string) ?>&sensor=false&key=<?php echo TMM::get_option("api_key_google")?>">
-
-<?php endif;
-
-}
-else{
-	echo "<h4>"; 
-	echo esc_html_e('Enter your Google Maps API key on Theme Options Page.', 'tmm_shortcodes');
-	echo "</h4>";
-}
-
-?>
