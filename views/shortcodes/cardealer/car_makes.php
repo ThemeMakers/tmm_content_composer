2fd618d25676aa40ee4b78d745d555396a30e137
oleksa <lelik4w@gmail.com>
2022-01-05 00:08:33 +0100

    improve car makes and car body shortcodes

--- views/shortcodes/cardealer/car_makes.php
+++ views/shortcodes/cardealer/car_makes.php
@@ -1,36 +1,38 @@
-<?php if (!defined('ABSPATH')) exit();
+<?php if (!defined('ABSPATH')) {
+    exit();
+}
 
 if (!empty($logos_list)) {
-	if (!is_array($logos_list)) {
-		$logos_list = explode(',', $logos_list);
-	}
+    if (!is_array($logos_list)) {
+        $logos_list = explode(',', $logos_list);
+    }
 } else {
-	$logos_list = '';
+    $logos_list = '';
 }
 
 $hide_empty = isset($hide_empty) ? $hide_empty : false;
 
-if ( !function_exists( 'get_terms' ) ) {
-	require_once ABSPATH . WPINC . '/taxonomy.php';
+if (!function_exists('get_terms')) {
+    require_once ABSPATH . WPINC . '/taxonomy.php';
 }
 
 // TODO: update the shortcode with the following feature extension
 $args = array(
-	'taxonomy'         => 'carproducer',
-	'orderby'          => 'none',
-	'order'            => 'ASC',
-	'include'          => $logos_list,
-	'hide_empty'       => $hide_empty,
-	'fields'           => 'all',
-	'parent'           => 0,
-	'hierarchical'     => true,
-	'pad_counts'       => true
+    'taxonomy' => 'carproducer',
+    'orderby' => 'none',
+    'order' => 'ASC',
+    'include' => $logos_list,
+    'hide_empty' => $hide_empty,
+    'fields' => 'all',
+    'parent' => 0,
+    'hierarchical' => true,
+    'pad_counts' => true,
 );
 
 $makes = get_terms($args);
 
 if (!isset($show_name)) {
-	$show_name = 1;
+    $show_name = 1;
 }
 //var_dump($logos_list);
 
@@ -43,57 +45,65 @@ if (!isset($show_name)) {
 <ul class="carproducers_list">
 
 	<?php
-	foreach ($makes as $make){
+foreach ($makes as $make) {
 
-//		var_dump($make->term_id);
+//        var_dump($make->term_id);
 
-		$image_name = strtolower($make->name);
-		$image_name = preg_replace( array('/\s/', '/ë/'), array('_', 'e'), $image_name );
-		$src = 'images/car_makes_logos/' . $image_name . '.svg';
+    $image_name = strtolower($make->name);
+    $image_name = preg_replace(array('/\s/', '/ë/'), array('_', 'e'), $image_name);
+    $src = 'images/car_makes_logos/' . $image_name . '.svg';
 
-		if ( !file_exists(TMM_CC_DIR . $src) ) {
-			$src = '';
-		} else {
-			$src = TMM_CC_URL . $src;
-		}
+    if (!file_exists(TMM_CC_DIR . $src)) {
+        $src = '';
+    } else {
+        $src = TMM_CC_URL . $src;
+    }
 
-		if (isset($show_only_with_logo) && $show_only_with_logo && !$src) {
-			continue;
-		}
+    if (isset($show_only_with_logo) && $show_only_with_logo && !$src) {
+        continue;
+    }
 
-		if($make->count > 0 || !$hide_empty){
-			?>
+    if ($make->count > 0 || !$hide_empty) {
+        ?>
 
-			<li class="cat-item-<?php echo esc_attr( $make->term_id ) ?><?php if($show_name){ ?> with-title<?php } ?>">
+			<li class="cat-item-<?php echo esc_attr($make->term_id) ?><?php if ($show_name) {?> with-title<?php }?>">
 
-			<?php if (!isset($show_link) || $show_link && $make->count > 0) { ?>
+			<?php if (!isset($show_link) || $show_link && $make->count > 0) {?>
+				<a title="<?php echo sprintf(esc_html__('View all ads filed under %s', 'tmm_content_composer'), $make->name); ?>" href="<?php echo get_term_link($make->slug, 'carproducer'); ?>" class="icon-link">
+			<?php }?>
+
+				<?php if ($show_logo && $src != '') {?>
+					<img src="<?php echo esc_attr($src) ?>" alt="<?php echo esc_html__($make->name, 'tmm_content_composer') ?>" />
+				<?php }?>
+
+			<?php if (!isset($show_link) || $show_link && $make->count > 0) {?>
+				</a>
+			<?php }?>
+
+			<?php if (!isset($show_link) || $show_link && $make->count > 0) {?>
 				<a title="<?php echo sprintf(esc_html__('View all ads filed under %s', 'tmm_content_composer'), $make->name); ?>" href="<?php echo get_term_link($make->slug, 'carproducer'); ?>">
-			<?php } ?>
+			<?php }?>
 
-				<?php if($show_logo && $src != ''){ ?>
-					<img src="<?php echo esc_attr( $src ) ?>" alt="<?php echo esc_html__( $make->name, 'tmm_content_composer' ) ?>" />
-				<?php } ?>
-
-				<?php if($show_name){ ?>
+				<?php if ($show_name) {?>
 					<span class="car-title">
 
 					<?php
-					echo esc_html__( $make->name, 'tmm_content_composer' );
-					echo (!isset($show_count) || $show_count) ? ' (' . $make->count . ')' : '';
-					?>
+echo esc_html__($make->name, 'tmm_content_composer');
+            echo (!isset($show_count) || $show_count) ? ' (' . $make->count . ')' : '';
+            ?>
 
 					</span>
-				<?php } ?>
+				<?php }?>
 
-			<?php if (!isset($show_link) || $show_link && $make->count > 0) { ?>
+			<?php if (!isset($show_link) || $show_link && $make->count > 0) {?>
 				</a>
-			<?php } ?>
+			<?php }?>
 
 			</li>
 
 		<?php
-		}
-	}
-	?>
+}
+}
+?>
 
 </ul>
--- views/shortcodes/cardealer/search_by_car_body.php
+++ views/shortcodes/cardealer/search_by_car_body.php
@@ -1,91 +1,91 @@
-<?php if (!defined('ABSPATH')) exit();
+<?php if (!defined('ABSPATH')) {
+    exit();
+}
 
 $car_bodies = array();
 $car_body_list = TMM_Ext_PostType_Car::$car_options['body'];
 $searching_page = get_permalink(TMM::get_option('searching_page', TMM_APP_CARDEALER_PREFIX));
 
 foreach ($car_body_list as $k => $body_name) {
-	$car_bodies[$k] = array(
-		'name' => $body_name,
-		'url' => add_query_arg( array('car_body'=> $k), $searching_page),
-		'count' => 0,
-		'icon' => file_exists(TMM_EXT_PATH . '/cardealer/images/car_body_icons/' . $k . '.svg')
-						? TMM_EXT_URI . '/cardealer/images/car_body_icons/' . $k . '.svg'
-						: TMM_EXT_URI . '/cardealer/images/car_body_icons/sedan.svg',
-	);
+    $car_bodies[$k] = array(
+        'name' => $body_name,
+        'url' => add_query_arg(array('car_body' => $k), $searching_page),
+        'count' => 0,
+        'icon' => file_exists(TMM_EXT_PATH . '/cardealer/images/car_body_icons/' . $k . '.svg')
+        ? TMM_EXT_URI . '/cardealer/images/car_body_icons/' . $k . '.svg'
+        : TMM_EXT_URI . '/cardealer/images/car_body_icons/sedan.svg',
+    );
 
-	if (!empty($show_count)) {
+    if (!empty($show_count)) {
 
-		$args = array(
-			'post_type' => TMM_Ext_PostType_Car::$slug,
-			'showposts' => -1,
-			'meta_query' => array(
-				array(
-					'key' => 'car_body',
-					'value' => $k,
-					'compare' => '='
-				),
-			),
-		);
+        $args = array(
+            'post_type' => TMM_Ext_PostType_Car::$slug,
+            'showposts' => -1,
+            'meta_query' => array(
+                array(
+                    'key' => 'car_body',
+                    'value' => $k,
+                    'compare' => '=',
+                ),
+            ),
+        );
 
-		if(!defined('ICL_LANGUAGE_CODE')){
-			$args['meta_query'][] = array(
-				'key' => '_icl_lang_duplicate_of',
-				'value' => '',
-				'compare' => 'NOT EXISTS'
-			);
-		}
+        if (!defined('ICL_LANGUAGE_CODE')) {
+            $args['meta_query'][] = array(
+                'key' => '_icl_lang_duplicate_of',
+                'value' => '',
+                'compare' => 'NOT EXISTS',
+            );
+        }
 
-		$query = new WP_Query($args);
-		$car_bodies[$k]['count'] = $query->post_count;
-		wp_reset_postdata();
+        $query = new WP_Query($args);
+        $car_bodies[$k]['count'] = $query->post_count;
+        wp_reset_postdata();
 
-	}
+    }
 }
 ?>
 
-<?php if (!empty($title)) { ?>
+<?php if (!empty($title)) {?>
 <div class="page-subheader">
 
-	<h3 class="section-title"><?php esc_html_e( $title, 'tmm_content_composer' ) ?></h3>
+	<h3 class="section-title"><?php esc_html_e($title, 'tmm_content_composer')?></h3>
 
 </div><!--/ .page-header-->
-<?php } ?>
+<?php }?>
 
 <ul class="lc-car-body-list">
 
-    <?php foreach ($car_bodies as $k => $body) { ?>
+    <?php foreach ($car_bodies as $k => $body) {?>
     <li>
 
-	    <?php if (!empty($enable_link)) { ?>
-	    <a href="<?php echo esc_url( $body['url'] ) ?>">
-	    <?php } ?>
+	    <?php if (!empty($enable_link)) {?>
+	    	<a href="<?php echo esc_url($body['url']) ?>" class="icon-link">
+	    <?php }?>
+		    <img src="<?php echo esc_url($body['icon']) ?>" alt="<?php echo esc_html__($body['name'], 'tmm_content_composer'); ?>" />
+		<?php if (!empty($enable_link)) {?>
+	    	</a>
+	    <?php }?>
 
-		    <img src="<?php echo esc_url( $body['icon'] ) ?>" alt="<?php echo esc_html__( $body['name'], 'tmm_content_composer' ); ?>" />
+		<?php if (!empty($show_name)) {?><h6><?php }?>
 
-		    <?php if (!empty($show_name)) { ?>
-		    <h6>
-			<?php } ?>
+			<?php if (!empty($enable_link)) {?><a href="<?php echo esc_url($body['url']) ?>"><?php }?>
 
-		    <?php
-		    if (!empty($show_name)) {
-		        echo esc_html__( $body['name'], 'tmm_content_composer' );
-		    }
+<?php
+if (!empty($show_name)) {
+    echo esc_html__($body['name'], 'tmm_content_composer');
+}
 
-		    if (!empty($show_count)) {
-			    echo ' (' . (int) $body['count'] . ')';
-		    }
-		    ?>
+    if (!empty($show_count)) {
+        echo ' (' . (int) $body['count'] . ')';
+    }
+    ?>
 
-			<?php if (!empty($show_name)) { ?>
-		    </h6>
-	        <?php } ?>
+			<?php if (!empty($enable_link)) {?></a><?php }?>
 
-	    <?php if (!empty($enable_link)) { ?>
-	    </a>
-	    <?php } ?>
+		<?php if (!empty($show_name)) {?></h6><?php }?>
 
     </li>
-    <?php } ?>
+    <?php }?>
 
 </ul>
