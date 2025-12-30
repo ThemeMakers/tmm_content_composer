d4388d33fedb33499dd533c572c4af3887b62ad2
Oleksiy Ostapenko <lelik4w@gmail.com>
2025-12-30 02:05:37 +0100

    improve carlocations shortcode

--- views/shortcodes/cardealer/car_locations.php
+++ views/shortcodes/cardealer/car_locations.php
@@ -2,87 +2,74 @@
 
 $terms = TMM_Ext_PostType_Car::get_locations(0);
 $searching_page = get_permalink(TMM::get_option('searching_page', TMM_APP_CARDEALER_PREFIX));
+
+$base_location_url = esc_url($searching_page);
 ?>
 
 <ul class="list-entry carlocations_list list">
 
-	<?php
-	foreach ($terms as $term) {
-		$states = array();
-		if ($location_level > 1) {
-			$states = Carlocation_List_Table::get_children_items(array($term->id));
-		}
+	<?php foreach ($terms as $term) : ?>
+		<?php
+		$states = ($location_level > 1) ? Carlocation_List_Table::get_children_items(array($term->id)) : array();
 		$car_count = TMM_Ext_PostType_Car::get_cars_count_by_locationid($term->id, 1);
 
-		if ($car_count > 0 || !$hide_empty) {
-	?>
+		if ($car_count <= 0 && $hide_empty) {
+			continue;
+		}
+		?>
 
-			<li class="cat-item cat-item-<?php echo $term->id ?>">
-				<a title="<?php echo sprintf(__('View all posts filed under %s', 'tmm_content_composer'), $term->name); ?>" href="<?php echo $searching_page; ?>?carlocation=<?php echo $term->id ?>"><?php esc_html_e($term->name, 'tmm_content_composer'); ?></a> (<?php echo $car_count; ?>)&#x200E;
-				<?php
-				if (count($states)) {
-				?>
+		<li class="cat-item cat-item-<?php echo (int) $term->id; ?>">
+			<a title="<?php echo esc_attr(sprintf(__('View all posts filed under %s', 'tmm_content_composer'), $term->name)); ?>"
+				href="<?php echo $base_location_url; ?>"
+				data-carlocation="<?php echo (int) $term->id; ?>"
+				data-carlocation-labels="<?php echo esc_attr($term->name); ?>"
+				rel="nofollow"><?php echo esc_html($term->name); ?></a>
+			(<?php echo (int) $car_count; ?>)&#x200E;
 
-					<ul class="list-entry carlocations_list list">
-
+			<?php if (!empty($states)) : ?>
+				<ul class="list-entry carlocations_list list">
+					<?php foreach ($states as $state) : ?>
 						<?php
-						foreach ($states as $state) {
-							$cities = array();
-							if ($location_level > 2) {
-								$cities = Carlocation_List_Table::get_children_items(array($state->id));
-							}
-							$car_count = TMM_Ext_PostType_Car::get_cars_count_by_locationid($state->id, 2);
+						$cities = ($location_level > 2) ? Carlocation_List_Table::get_children_items(array($state->id)) : array();
+						$car_count = TMM_Ext_PostType_Car::get_cars_count_by_locationid($state->id, 2);
 
-							if ($car_count > 0 || !$hide_empty) {
-						?>
-
-								<li class="cat-item cat-item-<?php echo $state->id ?>">
-									<a title="<?php echo sprintf(__('View all posts filed under %s', 'tmm_content_composer'), $state->name); ?>" href="<?php echo $searching_page; ?>?carlocation=<?php echo $term->id . ',' . $state->id ?>"><?php esc_html_e($state->name, 'tmm_content_composer'); ?></a> (<?php echo $car_count; ?>)&#x200E;
-									<?php
-									if (count($cities)) {
-									?>
-
-										<ul class="list-entry carlocations_list list">
-
-											<?php
-											foreach ($cities as $city) {
-												$car_count = TMM_Ext_PostType_Car::get_cars_count_by_locationid($city->id, 3);
-
-												if ($car_count > 0 || !$hide_empty) {
-											?>
-
-													<li class="cat-item cat-item-<?php echo $city->id ?>">
-														<a title="<?php echo sprintf(__('View all posts filed under %s', 'tmm_content_composer'), $city->name); ?>" href="<?php echo $searching_page; ?>?carlocation=<?php echo $term->id . ',' . $state->id . ',' . $city->id ?>"><?php esc_html_e($city->name, 'tmm_content_composer'); ?></a> (<?php echo $car_count; ?>)&#x200E;
-
-													</li>
-
-											<?php
-												}
-											}
-											?>
-
-										</ul>
-
-									<?php
-									}
-									?>
-								</li>
-
-						<?php
-							}
+						if ($car_count <= 0 && $hide_empty) {
+							continue;
 						}
 						?>
+						<li class="cat-item cat-item-<?php echo (int) $state->id; ?>">
+							<a title="<?php echo esc_attr(sprintf(__('View all posts filed under %s', 'tmm_content_composer'), $state->name)); ?>"
+								href="<?php echo $base_location_url; ?>"
+								data-carlocation="<?php echo esc_attr($term->id . ',' . $state->id); ?>"
+								data-carlocation-labels="<?php echo esc_attr($term->name . ',' . $state->name); ?>"
+								rel="nofollow"><?php echo esc_html($state->name); ?></a>
+							(<?php echo (int) $car_count; ?>)&#x200E;
 
-					</ul>
-
-				<?php
-				}
-				?>
-			</li>
-
-	<?php
-		}
-	}
-	?>
+							<?php if (!empty($cities)) : ?>
+								<ul class="list-entry carlocations_list list">
+									<?php foreach ($cities as $city) : ?>
+										<?php
+										$car_count = TMM_Ext_PostType_Car::get_cars_count_by_locationid($city->id, 3);
+										if ($car_count <= 0 && $hide_empty) {
+											continue;
+										}
+										?>
+										<li class="cat-item cat-item-<?php echo (int) $city->id; ?>">
+											<a title="<?php echo esc_attr(sprintf(__('View all posts filed under %s', 'tmm_content_composer'), $city->name)); ?>"
+												href="<?php echo $base_location_url; ?>"
+												data-carlocation="<?php echo esc_attr($term->id . ',' . $state->id . ',' . $city->id); ?>"
+												data-carlocation-labels="<?php echo esc_attr($term->name . ',' . $state->name . ',' . $city->name); ?>"
+												rel="nofollow"><?php echo esc_html($city->name); ?></a>
+											(<?php echo (int) $car_count; ?>)&#x200E;
+										</li>
+									<?php endforeach; ?>
+								</ul>
+							<?php endif; ?>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+			<?php endif; ?>
+		</li>
+	<?php endforeach; ?>
 
 </ul>
